d0bd4aadf4e30fc26c4070945af29e8e62524911
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2009-11-19 03:39:37 +0000

    tests updated per refactoring
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3400 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: db85029ba3609e0df6a06c9536929a9bdac458a5

--- examples/stanfordBlacklight/test/src/edu/stanford/PublicationTests.java
+++ examples/stanfordBlacklight/test/src/edu/stanford/PublicationTests.java
@@ -14,7 +14,8 @@ import org.xml.sax.SAXException;
 import org.junit.*;
 
 import org.solrmarc.solr.DocumentProxy;
-import edu.stanford.PubDateGroupValues;
+
+import edu.stanford.enumValues.PubDateGroup;
 
 /**
  * junit4 tests for Stanford University publication fields for blacklight index
@@ -438,10 +439,10 @@ public class PublicationTests extends AbstractStanfordBlacklightTest
 		docIds.add("pubDate2010");
 		docIds.add("z2009");
 		docIds.add("b2008");
-		assertSearchResults(fldName, "\"" + PubDateGroupValues.THIS_YEAR.toString() + "\"", docIds);
+		assertSearchResults(fldName, "\"" + PubDateGroup.THIS_YEAR.toString() + "\"", docIds);
 		docIds.add("v2007");
 		docIds.add("z2006");
-		assertSearchResults(fldName, "\"" + PubDateGroupValues.LAST_3_YEARS.toString() + "\"", docIds);
+		assertSearchResults(fldName, "\"" + PubDateGroup.LAST_3_YEARS.toString() + "\"", docIds);
 		docIds.add("j2005");
 		docIds.add("q2001");
 		docIds.add("f2000");
@@ -452,7 +453,7 @@ public class PublicationTests extends AbstractStanfordBlacklightTest
 		docIds.add("o20uu");
 		docIds.add("pubDate0059");  // 2005
 		docIds.add("pubDate0204");  // 2004
-		assertSearchResults(fldName, "\"" + PubDateGroupValues.LAST_10_YEARS.toString() + "\"", docIds);
+		assertSearchResults(fldName, "\"" + PubDateGroup.LAST_10_YEARS.toString() + "\"", docIds);
 		docIds.add("c1998");
 		docIds.add("e1997");
 		docIds.add("m1991");
@@ -466,7 +467,7 @@ public class PublicationTests extends AbstractStanfordBlacklightTest
 		docIds.add("p19uu");
 		docIds.add("pubDate0197-1");
 		docIds.add("pubDate0197-2");
-		assertSearchResults(fldName, "\"" + PubDateGroupValues.LAST_50_YEARS.toString() + "\"", docIds);
+		assertSearchResults(fldName, "\"" + PubDateGroup.LAST_50_YEARS.toString() + "\"", docIds);
 
 		docIds.clear();
 		docIds.add("pubDate00uu");   // "1st century"
@@ -494,7 +495,7 @@ public class PublicationTests extends AbstractStanfordBlacklightTest
 		docIds.add("pubDate0965"); // should be 1965
 		docIds.add("pubDate0980"); // should be 1980
 		docIds.add("pubDate0999"); // should be 1999
-		assertSearchResults(fldName, "\"" + PubDateGroupValues.MORE_THAN_50_YEARS_AGO.toString() + "\"", docIds);
+		assertSearchResults(fldName, "\"" + PubDateGroup.MORE_THAN_50_YEARS_AGO.toString() + "\"", docIds);
 	}
 
 
