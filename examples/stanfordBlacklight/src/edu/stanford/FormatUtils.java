f4b38610e60240a4a05872922803e7f982b16f7d
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2009-12-01 01:36:57 +0000

    refactor - move format checks serial pubs into separate methods in FormatUtil
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3458 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: 797389802b37d6c1a0b185259d9a039a5905d6fa

--- examples/stanfordBlacklight/src/edu/stanford/FormatUtils.java
+++ examples/stanfordBlacklight/src/edu/stanford/FormatUtils.java
@@ -24,7 +24,7 @@ public class FormatUtils {
 	 * Assign formats per algorithm and marc bib record
 	 */
 	@SuppressWarnings("unchecked")
-	static Set<String> setFormats(String leaderStr, ControlField cf008, VariableField f006, Set<Item> itemSet, Record record) 
+	static Set<String> getFormats(String leaderStr, ControlField cf008, VariableField f006, Set<Item> itemSet, Record record) 
 	{
 		Set<String> result = new HashSet<String>();
 
@@ -38,56 +38,6 @@ public class FormatUtils {
 
 		// assign formats based on leader chars 06, 07 and chars in 008
 		char leaderChar07 = leaderStr.charAt(7);
-		char leaderChar06 = leaderStr.charAt(6);
-		switch (leaderChar06) {
-		case 'a':
-			if (leaderChar07 == 'a' || leaderChar07 == 'm')
-				result.add(Format.BOOK.toString());
-			break;
-		case 'b':
-		case 'p':
-			result.add(Format.MANUSCRIPT_ARCHIVE.toString());
-			break;
-		case 'c':
-		case 'd':
-			result.add(Format.MUSIC_SCORE.toString());
-			break;
-		case 'e':
-		case 'f':
-			result.add(Format.MAP_GLOBE.toString());
-			break;
-		case 'g':
-			// look for m or v in 008 field, char 33 (count starts at 0)
-			if (cf008 != null && cf008.find("^.{33}[mv]"))
-				result.add(Format.VIDEO.toString());
-			break;
-		case 'i':
-			result.add(Format.SOUND_RECORDING.toString());
-			break;
-		case 'j':
-			result.add(Format.MUSIC_RECORDING.toString());
-			break;
-		case 'k':
-    		// look for i, k, p, s or t in 008 field, char 33 (count starts at 0)
-			if (cf008 != null && cf008.find("^.{33}[ikpst]"))
-				result.add(Format.IMAGE.toString());
-			break;
-		case 'm':
-			// look for a in 008 field, char 26 (count starts at 0)
-			if (cf008 != null && cf008.find("^.*{26}a"))
-				result.add(Format.COMPUTER_FILE.toString());
-			break;
-		case 'o': // instructional kit
-			result.add(Format.OTHER.toString());
-			break;
-		case 'r': // object
-			result.add(Format.OTHER.toString());
-			break;
-		case 't':
-			if (leaderChar07 == 'a' || leaderChar07 == 'm')
-				result.add(Format.BOOK.toString());
-			break;
-		} // end switch
 
 		if (result.isEmpty() || result.size() == 0) {
 			// look for serial publications - leader/07 s
@@ -195,6 +145,176 @@ public class FormatUtils {
 		return result;
 	}
 
+	
+	/**
+	 * Assign formats based on leader chars 06, 07 and chars in 008
+	 * 
+	 * Algorithms for formats are currently in email  message from Vitus Tang to
+	 *  Naomi Dushay, cc Phil Schreur, Margaret Hughes, and Jennifer Vine 
+	 *  dated July 23, 2008.
+	 *  
+	 * @param leaderStr - the leader field, as a String
+	 * @param cf008 - the 008 field as a ControlField object
+	 * @param Set of Strings containing Format enum values per the given data
+	 */
+	@SuppressWarnings("unchecked")
+	static Set<String> getFormatsPerLdrAnd008(String leaderStr, ControlField cf008) 
+	{
+		Set<String> result = new HashSet<String>();
+
+		// Note: MARC21 documentation refers to char numbers that are 0 based,
+		// just like java string indexes, so char "06" is at index 6, and is
+		// the seventh character of the field
+
+		// assign formats based on leader chars 06, 07 and chars in 008
+		char leaderChar07 = leaderStr.charAt(7);
+		char leaderChar06 = leaderStr.charAt(6);
+		switch (leaderChar06) {
+		case 'a':
+			if (leaderChar07 == 'a' || leaderChar07 == 'm')
+				result.add(Format.BOOK.toString());
+			break;
+		case 'b':
+		case 'p':
+			result.add(Format.MANUSCRIPT_ARCHIVE.toString());
+			break;
+		case 'c':
+		case 'd':
+			result.add(Format.MUSIC_SCORE.toString());
+			break;
+		case 'e':
+		case 'f':
+			result.add(Format.MAP_GLOBE.toString());
+			break;
+		case 'g':
+			// look for m or v in 008 field, char 33 (count starts at 0)
+			if (cf008 != null && cf008.find("^.{33}[mv]"))
+				result.add(Format.VIDEO.toString());
+			break;
+		case 'i':
+			result.add(Format.SOUND_RECORDING.toString());
+			break;
+		case 'j':
+			result.add(Format.MUSIC_RECORDING.toString());
+			break;
+		case 'k':
+    		// look for i, k, p, s or t in 008 field, char 33 (count starts at 0)
+			if (cf008 != null && cf008.find("^.{33}[ikpst]"))
+				result.add(Format.IMAGE.toString());
+			break;
+		case 'm':
+			// look for a in 008 field, char 26 (count starts at 0)
+			if (cf008 != null && cf008.find("^.*{26}a"))
+				result.add(Format.COMPUTER_FILE.toString());
+			break;
+		case 'o': // instructional kit
+			result.add(Format.OTHER.toString());
+			break;
+		case 'r': // object
+			result.add(Format.OTHER.toString());
+			break;
+		case 't':
+			if (leaderChar07 == 'a' || leaderChar07 == 'm')
+				result.add(Format.BOOK.toString());
+			break;
+		} // end switch
+
+		return result;
+	}
+	
+	
+	/**
+	 * Assign format based on Serial publications - leader/07 s
+	 * 
+	 * Algorithms for formats are currently in email  message from Vitus Tang to
+	 *  Naomi Dushay, cc Phil Schreur, Margaret Hughes, and Jennifer Vine 
+	 *  dated July 23, 2008.
+	 *  
+	 * @param leaderStr - the leader field, as a String
+	 * @param cf008 - the 008 field as a ControlField object
+	 * @param Set of Strings containing Format enum values per the given data
+	 */
+	@SuppressWarnings("unchecked")
+	static String getSerialFormat(char leaderChar07, ControlField cf008) 
+	{
+		String result = null;
+		if (leaderChar07 == 's' && cf008 != null) {
+			char c21 = ((ControlField) cf008).getData().charAt(21);
+			result = getSerialFormatFromChar(c21);
+		}
+		if (result == null) {
+			
+		}
+		
+		return null;
+
+	}
+	
+	/**
+	 * Assign format if leader/07 s indicates a Serial format and the 
+	 *  008 21st char has a desirable value.
+	 * 
+	 * @param leaderStr - the leader field, as a String
+	 * @param cf008 - the 008 field as a ControlField object
+	 * @return String containing Format enum value per the given data, or null
+	 */
+	@SuppressWarnings("unchecked")
+	static String getSerialFormatLdr07s(char leaderChar07, ControlField cf008) 
+	{
+		if (leaderChar07 == 's' && cf008 != null) {
+			char c21 = ((ControlField) cf008).getData().charAt(21);
+			return getSerialFormatFromChar(c21);
+		}
+		return null;
+	}
+	
+	// look for serial publications 006/00 s
+	
+	/**
+	 * Assign format if 006 starts with 's' (?) and 4th char has a desirable
+	 *  value.
+	 * 
+	 * @param f006 - 006 as a VariableField object
+	 * @return String containing Format enum value per the given data, or null
+	 */
+	static String getSerialFormat006(VariableField f006) 
+	{
+		if (f006 != null && f006.find("^[s]")) {
+			char c04 = ((ControlField) f006).getData().charAt(4);
+			String format = getSerialFormatFromChar(c04);
+			if (format != null)
+				return format;
+			if (c04 == ' ')
+				return Format.JOURNAL_PERIODICAL.toString();
+		}
+		return null;
+		
+	}
+	
+	
+	/**
+	 * given a character assumed to be the 21st character (zero-based) from
+	 *  the 008 field or the 4th char from an 006 field, return the format 
+	 *  (assuming that there is an indication that the record is for a serial).
+	 *  return null if no format is determined.
+	 */
+	private static String getSerialFormatFromChar(char ch) {
+		switch (ch) {
+//			case 'd': // updating database (ignore)
+//				break;
+//			case 'l': // updating looseleaf (ignore)
+//				break;
+			case 'm': // monographic series
+				return Format.BOOK.toString();
+			case 'n':
+				return Format.NEWSPAPER.toString();
+			case 'p':
+				return Format.JOURNAL_PERIODICAL.toString();
+//			case 'w': // web site
+//				break;
+		}
+		return null;		
+	}
 	
 	/**
 	 * @param record
