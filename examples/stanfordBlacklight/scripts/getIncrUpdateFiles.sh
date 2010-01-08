2a4d578242c0f3077ef9f72fcff36250f1868dd8
blacklight <blacklight@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-01-08 22:13:38 +0000

    incr update fetch script now works with and without a date arg
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3588 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: 8bf0686e90a09ece848d801feb0fb8ee31be71bf

--- examples/stanfordBlacklight/scripts/getIncrUpdateFiles.sh
+++ examples/stanfordBlacklight/scripts/getIncrUpdateFiles.sh
@@ -8,13 +8,19 @@ REMOTE_DATA_DIR=/s/Dataload/SearchworkIncrement/Output
 LOCAL_DATA_DIR=/home/blacklight/data/unicorn
 LATEST_DATA_DIR=$LOCAL_DATA_DIR/latest/updates
 
-# check if dump is on unicorn box
-
+# get filename date
+if [ $1 ] ; then
+  COUNTS_FNAME=$1"_dates_counts"
+  DEL_KEYS_FNAME=$1"_ckeys_delete.del"
+  RECORDS_FNAME=$1"_uni_increment.marc"
+else
+  TODAY=`eval date +%y%m%d`
+  COUNTS_FNAME=$TODAY"_dates_counts"
+  DEL_KEYS_FNAME=$TODAY"_ckeys_delete.del"
+  RECORDS_FNAME=$TODAY"_uni_increment.marc"
+fi
+    
 #  sftp remote files with today's datestamp to "latest/updates"
-DATE=`eval date +%y%m%d`
-COUNTS_FNAME=$DATE"_dates_counts"
-DEL_KEYS_FNAME=$DATE"_ckeys_delete.del"
-RECORDS_FNAME=$DATE"_uni_increment.marc"
 
 sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$COUNTS_FNAME $LOCAL_DATA_DIR
 sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$DEL_KEYS_FNAME $LATEST_DATA_DIR/
