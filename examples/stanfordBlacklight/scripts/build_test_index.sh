77a8a4c2831ac17a4f0b9e1e7862f42b46cd4a75
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-02-08 22:51:55 +0000

    updated script to build a test index from a file.
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3613 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: 6fadcb87ed9c726742e6b3b94579abb26d5b2d23

--- examples/stanfordBlacklight/scripts/build_test_index.sh
+++ examples/stanfordBlacklight/scripts/build_test_index.sh
@@ -5,30 +5,31 @@
 
 BLACKLIGHT_HOMEDIR=/home/blacklight
 SOLRMARC_BASEDIR=$BLACKLIGHT_HOMEDIR/solrmarc
-SITE_BASEDIR=..
-DIST_DIR=$SOLRMARC_BASEDIR/dist
 
+SOLRMARC_JAR=$SOLRMARC_BASEDIR/dist/swSolrMarc.jar
+
+SITE_BASEDIR=..
 RAW_DATA_DIR=$SITE_BASEDIR/test/data
 #RAW_DATA_DIR=$BLACKLIGHT_HOMEDIR/data/unicorn/latest
-#SOLR_DATA_DIR=$BLACKLIGHT_HOMEDIR/data/solr/dataBuild
-SOLR_DATA_DIR=$SITE_BASEDIR/test/data/solr/data
-SOLRMARC_JAR=$DIST_DIR/swSolrMarc.jar
 
 JAVA_HOME=/usr/lib/jvm/java
 
 # TODO: determine today's date and create log dir with today's date, with a suffix if necessary
 
-# temporary! - take an argument for the date of the log subdirectory
+# temporary! - take an argument for the name of the log subdirectory
 LOG_SUBDIR=$1
 
 # create new dist jar
-ant -buildfile $SOLRMARC_BASEDIR/build.xml build
+#ant -buildfile $SOLRMARC_BASEDIR/build.xml build
 
 # get index directory ready 
-mv $SOLR_DATA_DIR/index $SOLR_DATA_DIR/index_b4_$LOG_SUBDIR
-mv $SOLR_DATA_DIR/spellchecker $SOLR_DATA_DIR/spellchecker_b4_$LOG_SUBDIR
-mv $SOLR_DATA_DIR/spellcheckerFile $SOLR_DATA_DIR/spellcheckerFile_b4_$LOG_SUBDIR
-mv $SOLR_DATA_DIR/spellcheckerJaroWin $SOLR_DATA_DIR/spellcheckerJaroWin_b4_$LOG_SUBDIR
+#SOLR_DATA_DIR=$BLACKLIGHT_HOMEDIR/data/solr/dataBuild
+SOLR_DATA_DIR=$SITE_BASEDIR/test/data/solr/data
+
+#mv $SOLR_DATA_DIR/index $SOLR_DATA_DIR/index_b4_$LOG_SUBDIR
+#mv $SOLR_DATA_DIR/spellchecker $SOLR_DATA_DIR/spellchecker_b4_$LOG_SUBDIR
+#mv $SOLR_DATA_DIR/spellcheckerFile $SOLR_DATA_DIR/spellcheckerFile_b4_$LOG_SUBDIR
+#mv $SOLR_DATA_DIR/spellcheckerJaroWin $SOLR_DATA_DIR/spellcheckerJaroWin_b4_$LOG_SUBDIR
 
 # create log directory
 LOG_PARENT_DIR=$RAW_DATA_DIR/logs
