01723613922a4fb0b9f31f1967d9f649382f5017
blacklight <blacklight@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-05-04 18:40:50 +0000

    fix datestamp on sysmsgs filename
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3670 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: b331b580f1bccaa15a4d94b7195dee46b0614b55

--- examples/stanfordBlacklight/scripts/jetty_start.sh
+++ examples/stanfordBlacklight/scripts/jetty_start.sh
@@ -11,10 +11,11 @@ JAVA_HOME=/usr/lib/jvm/java
 
 JAVA_OPTS="-server -Xmx12g -Xms12g -d64 -XX:+UseParallelGC -XX:+AggressiveOpts -XX:NewRatio=5"
 
-TODAY=`eval date +%F`
-HOUR=`eval date +%T`
-
 #LOG_OPTS=-Djava.util.logging.config.file=$BLACKLIGHT_HOMEDIR/jetty/solr/logging.properties
 
-#nohup $JAVA_HOME/bin/java $JAVA_OPTS $LOG_OPTS -Dsolr.data.dir=$SOLR_DATA_DIR -jar start.jar &>logs/sysmsgs_started_$TODAY_$HOUR.txt
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -Dsolr.data.dir=$SOLR_DATA_DIR -jar start.jar &>logs/sysmsgs_started_$TODAY_$HOUR.txt
+NOW=`eval date +"%F_%T"`
+
+SYSMSG_FNAME=sysmsgs_started_$NOW.txt
+
+#nohup $JAVA_HOME/bin/java $JAVA_OPTS $LOG_OPTS -Dsolr.data.dir=$SOLR_DATA_DIR -jar start.jar &>logs/$SYSMSG_FNAME
+nohup $JAVA_HOME/bin/java $JAVA_OPTS -Dsolr.data.dir=$SOLR_DATA_DIR -jar start.jar &>logs/$SYSMSG_FNAME
