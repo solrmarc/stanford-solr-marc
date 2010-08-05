9182c69a0f2ee18228e5fae5fdd7c6779d572553
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-08-05 23:03:23 +0000

    retain scripts that Bob Haschart deleted in his eagerness to get new build working for stanfordBlacklight example.
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3688 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: 32b89ea8edefd9669693263a60cb44b01a3bbbf8

---
+++ examples/stanfordBlacklight/scripts/getSirsiIncr.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# getSirsiIncr.sh
+# Pull over the latest incremental update files from Sirsi.  
+#  Naomi Dushay 2010-01-08
+
+REMOTE_DATA_DIR=/s/Dataload/SearchworkIncrement/Output
+
+LOCAL_DATA_DIR=/data/sirsi
+LATEST_DATA_DIR=$LOCAL_DATA_DIR/latest/updates
+
+# get filename date
+if [ $1 ] ; then
+  COUNTS_FNAME=$1"_dates_counts"
+  DEL_KEYS_FNAME=$1"_ckeys_delete.del"
+  RECORDS_FNAME=$1"_uni_increment.marc"
+else
+  TODAY=`eval date +%y%m%d`
+  COUNTS_FNAME=$TODAY"_dates_counts"
+  DEL_KEYS_FNAME=$TODAY"_ckeys_delete.del"
+  RECORDS_FNAME=$TODAY"_uni_increment.marc"
+fi
+    
+#  sftp remote files with today's datestamp to "latest/updates"
+
+sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$COUNTS_FNAME $LOCAL_DATA_DIR
+sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$DEL_KEYS_FNAME $LATEST_DATA_DIR/
+sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$RECORDS_FNAME $LATEST_DATA_DIR/
+
+exit 0
