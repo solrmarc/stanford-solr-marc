f8a4f68492617c6ee72f51daab7457e557569917
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-08-08 21:43:03 +0000

    working on script for test index to get command correct for production scripts
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3694 e0eb17dc-060e-4555-9e78-273a5d168c06

--- examples/stanfordBlacklight/scripts/build_test_index.sh
+++ examples/stanfordBlacklight/scripts/build_test_index.sh
@@ -5,12 +5,9 @@
 
 BLACKLIGHT_HOMEDIR=/home/blacklight
 SOLRMARC_BASEDIR=$BLACKLIGHT_HOMEDIR/solrmarc
+SITE_BASEDIR=$SOLRMARC_BASEDIR/examples/stanfordBlacklight
 
-SOLRMARC_JAR=$SOLRMARC_BASEDIR/dist/swSolrMarc.jar
-
-SITE_BASEDIR=..
 RAW_DATA_DIR=$SITE_BASEDIR/test/data
-#RAW_DATA_DIR=$BLACKLIGHT_HOMEDIR/data/unicorn/latest
 
 JAVA_HOME=/usr/lib/jvm/java
 
@@ -19,26 +16,31 @@ JAVA_HOME=/usr/lib/jvm/java
 # temporary! - take an argument for the name of the log subdirectory
 LOG_SUBDIR=$1
 
-# create new dist jar
+# create new dist files
+ant -buildfile $SOLRMARC_BASEDIR/build.xml -Dexample.configuration=stanfordBlacklight -Dinput.continue.processing.with.not.uptodate=c dist
 #ant -buildfile $SOLRMARC_BASEDIR/build.xml build
 
-# get index directory ready 
-#SOLR_DATA_DIR=$BLACKLIGHT_HOMEDIR/data/solr/dataBuild
-SOLR_DATA_DIR=$SITE_BASEDIR/test/data/solr/data
+# set up the classpath
+$DIST_DIR = $SOLRMARC_BASEDIR/dist
+$SITE_JAR = $DIST_DIR/StanfordIndexer.jar
+$CORE_JAR = $DIST_DIR/dist/SolrMarc.jar
+$CP = $SITE_JAR:$CORE_JAR:$DIST_DIR
 
-#mv $SOLR_DATA_DIR/index $SOLR_DATA_DIR/index_b4_$LOG_SUBDIR
+# get index directory ready 
+SOLR_DATA_DIR=$SOLRMARC_BASEDIR/local_build/test/solr/data
+mv $SOLR_DATA_DIR/index $SOLR_DATA_DIR/index_b4_$LOG_SUBDIR
+# no longer using spellcheck as of 2010-01 (?)
 #mv $SOLR_DATA_DIR/spellchecker $SOLR_DATA_DIR/spellchecker_b4_$LOG_SUBDIR
 #mv $SOLR_DATA_DIR/spellcheckerFile $SOLR_DATA_DIR/spellcheckerFile_b4_$LOG_SUBDIR
 #mv $SOLR_DATA_DIR/spellcheckerJaroWin $SOLR_DATA_DIR/spellcheckerJaroWin_b4_$LOG_SUBDIR
 
 # create log directory
-LOG_PARENT_DIR=$RAW_DATA_DIR/logs
-mkdir $LOG_PARENT_DIR
+LOG_PARENT_DIR=$SOLRMARC_BASEDIR/local_build/test/solr/logs
 LOG_DIR=$LOG_PARENT_DIR/$LOG_SUBDIR
 mkdir $LOG_DIR
 
 # index the file
-#java -Xmx1g -Xms1g -Dsolr.data.dir=$SOLR_DATA_DIR -jar $SOLRMARC_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
-java -Xmx1g -Xms1g -Dsolr.data.dir=$SOLR_DATA_DIR -Dsolr.optimize_at_end="true" -jar $SOLRMARC_JAR $RAW_DATA_DIR/unicornWHoldings.mrc &>$LOG_DIR/log.txt
+java -Xmx1g -Xms1g -Dsolr.data.dir=$SOLR_DATA_DIR -cp $CP -Dsolrmarc.main.class="org.solrmarc.marc.MarcImporter" -jar $CORE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
+#java -Xmx1g -Xms1g -Dsolr.data.dir=$SOLR_DATA_DIR -cp $CP -Dsolr.optimize_at_end="true" -jar $CORE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
 
 exit 0
