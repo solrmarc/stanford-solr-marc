10148e4c8028f85806b70a3f37340337e1547f5a
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-08-08 23:17:17 +0000

    updates to scripts for solrmarc 2.1 vendor drop.
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3704 e0eb17dc-060e-4555-9e78-273a5d168c06

--- examples/stanfordBlacklight/scripts/pullThenIndexSirsiIncr.sh
+++ examples/stanfordBlacklight/scripts/pullThenIndexSirsiIncr.sh
@@ -33,20 +33,27 @@ sftp -o 'IdentityFile=~/.ssh/id_rsa' apache@jenson:$REMOTE_DATA_DIR/$RECORDS_FNA
 #########
 
 JAVA_HOME=/usr/lib/jvm/java
+
 SOLR_DATA_DIR=/data/solr
+
+# set up the classpath
 BLACKLIGHT_HOMEDIR=/home/blacklight
-SOLRMARC_JAR=$BLACKLIGHT_HOMEDIR/solrmarc/dist/swSolrMarc.jar
-
-REC_FNAME=$LATEST_DATA_DIR/$RECORDS_FNAME
-
-DEL_ARG="-Dmarc.ids_to_delete="$LATEST_DATA_DIR/$DEL_KEYS_FNAME
+SOLRMARC_BASEDIR=$BLACKLIGHT_HOMEDIR/solrmarc
+DIST_DIR=$SOLRMARC_BASEDIR/dist
+SITE_JAR=$DIST_DIR/StanfordIndexer.jar
+CORE_JAR=$DIST_DIR/SolrMarc.jar
+CP=$SITE_JAR:$CORE_JAR:$DIST_DIR
 
 # create log directory
 LOG_DIR=$LATEST_DATA_DIR/logs
 mkdir -p $LOG_DIR
 
+REC_FNAME=$LATEST_DATA_DIR/$RECORDS_FNAME
+
+DEL_ARG="-Dmarc.ids_to_delete="$LATEST_DATA_DIR/$DEL_KEYS_FNAME
+
 # index the files
-java -Xmx4g -Xms4g -Dsolr.data.dir=$SOLR_DATA_DIR $DEL_ARG -jar $SOLRMARC_JAR $REC_FNAME &>$LOG_DIR/$RECORDS_FNAME".txt"
+java -Xmx4g -Xms4g -Dsolr.data.dir=$SOLR_DATA_DIR $DEL_ARG -cp $CP -jar $CORE_JAR $REC_FNAME &>$LOG_DIR/$RECORDS_FNAME".txt"
 
 echo " "
 cat $LOG_DIR/$RECORDS_FNAME".txt"
