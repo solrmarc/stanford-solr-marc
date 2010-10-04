a7a77f2d6c4069ee88362412d5a3af251ec23c04
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-10-04 03:10:33 +0000

    added some MarcUtils methods AND UNIT TESTS.
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3728 e0eb17dc-060e-4555-9e78-273a5d168c06
    
    Former-commit-id: fa6e2edd64b8d0dc735d852eb4a8ec517fb43a12

--- lib/solrmarc/src/org/solrmarc/tools/MarcUtils.java
+++ lib/solrmarc/src/org/solrmarc/tools/MarcUtils.java
@@ -12,12 +12,78 @@ public class MarcUtils {
     private MarcUtils(){ }
 
     /**
+     * Return an ordered list of marc4j DataField objects for the range of marc tags 
+     *  indicated by the (inclusive) bounds.  Tags must be 010 or higher.
+     * @param record - marc record object
+     * @param lowerFieldBoundStr - the "lowest" marc field to include (e.g. 600) - inclusive
+     * @param upperFieldBoundStr - the "highest" marc field to include (e.g. 699) - inclusive
+     * @return List of DataField objects matching the field range.
+     */
+    @SuppressWarnings("unchecked")
+    public static List<DataField> getDataFieldsInRange(final Record record, String lowerFieldBoundStr, String upperFieldBoundStr)
+    {
+    	List<DataField> resultList = new ArrayList<DataField>();
+        int lowerBound = Utils.parseIntNoNFE(lowerFieldBoundStr, -1);
+        int upperBound = Utils.parseIntNoNFE(upperFieldBoundStr, -1);
+
+        List<DataField> fields = record.getDataFields();
+        for (DataField field : fields)
+        {
+            // This will ignore any "code" fields and only use textual fields
+            int tag = Utils.parseIntNoNFE(field.getTag(), -1);
+            if ((tag >= lowerBound) && (tag <= upperBound))
+            {
+            	resultList.add(field);
+            }
+        }
+        return resultList;
+    }
+    
+    /**
+     * return the ordered List of strings from the indicated subfields from the given
+     *  set of marc DataField objects.
+     * @param dataFields a Set of DataField objects to be processed
+     * @param subfldsStr a string of characters identifying which subfield
+     *  values to put in the result set. 
+     * @return List of String objects containing values in desired subfields
+     */
+    @SuppressWarnings("unchecked")
+	public static List<String> getSubfieldStrings(final List<DataField> dataFields, String subfldsStr)
+    {
+    	List<String> resultSet = new ArrayList<String>();
+    	for (DataField df : dataFields) {
+    		if (subfldsStr.length() == 1)
+    		{
+    			// get all instances of the single subfield
+	            List<Subfield> subFlds = df.getSubfields(subfldsStr.charAt(0));
+	            for (Subfield sf : subFlds)
+	            {
+	                resultSet.add(sf.getData().trim());
+	            }
+    		}
+    		else
+	        {
+	            List<Subfield> subFlds = df.getSubfields();
+	            for (Subfield sf : subFlds)
+	            {
+	                if (subfldsStr.indexOf(sf.getCode()) != -1)
+	                {
+	                	resultSet.add(sf.getData().trim());
+	                }
+	            }
+	        }
+    	}
+    	return resultSet;
+    }
+    
+    
+    /**
      * For each occurrence of a marc field in the tags list, extract all 
      * subfield data from the field, place it in a single string (individual 
      * subfield data separated by spaces) and add the string to the result set.
      */
     @SuppressWarnings("unchecked")
-    public static final Set<String> getAllSubfields(final Record record, String[] tags) 
+    public static Set<String> getAllSubfields(final Record record, String[] tags) 
     {
         Set<String> result = new LinkedHashSet<String>();
 
@@ -50,7 +116,7 @@ public class MarcUtils {
      * @param code - code of subfield of interest
      * @return the contents of the subfield, if it exists; null otherwise
      */
-    public static final String getSubfieldData(DataField df, char code) {
+    public static String getSubfieldData(DataField df, char code) {
         String result = null;
         if (df != null) {
             Subfield sf = df.getSubfield(code);
@@ -65,7 +131,7 @@ public class MarcUtils {
      *  contained in the data field
      */
     @SuppressWarnings("unchecked")
-    public static final List<String> getSubfieldStrings(DataField df, char code) {
+    public static List<String> getSubfieldStrings(DataField df, char code) {
         List<Subfield> listSubcode = df.getSubfields(code);
         List<String> vals = new ArrayList(listSubcode.size());
         for (Subfield s : listSubcode) {
---
+++ test/src/org/solrmarc/index/MarcUtilsUnitTests.java
@@ -0,0 +1,118 @@
+package org.solrmarc.index;
+
+import static org.solrmarc.tools.MarcUtils.*;
+
+import org.marc4j.marc.*;
+import org.marc4j.marc.impl.*;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.*;
+
+/**
+ * Unit tests for methods in org.solrmarc.tools.MarcUtils
+ * @author naomi
+ *
+ */
+public class MarcUtilsUnitTests {
+
+	/**
+	 * unit test for org.solrmarc.tools.MarcUtils.getDataFieldsInRange
+	 */
+@Test
+	public void testGetDataFieldsInRange()
+	{
+		Record rec = new NoSortRecordImpl();
+		
+		Subfield sf = new SubfieldImpl('a', "ignored");
+
+		String[] fldTagList = new String[]{"333", "321", "343", "002", "343", "345", "344", "010"};
+		for (int i = 0; i < fldTagList.length; i++) {
+			String tag = fldTagList[i];
+			DataField df = new DataFieldImpl(tag, ' ', ' ');
+			df.addSubfield(sf);
+			rec.addVariableField(df);
+		}
+	
+		List<DataField> dfResult = getDataFieldsInRange(rec, "320", "344");
+		List<String> resultTags = new ArrayList<String>(5);
+		for (DataField df : dfResult) {
+			resultTags.add(df.getTag());
+		}
+		
+		// bounds
+		assertTrue("lower bound field (321) should be included", resultTags.contains("321"));
+		assertTrue("upper bound field (344) should be included", resultTags.contains("344"));
+		assertTrue("field out of bounds (345) should not be included", !resultTags.contains("345"));
+		// with control field numbers
+		assertTrue("field out of bounds (002) should not be included", !resultTags.contains("002"));
+		
+		// fields in order encountered
+		assertTrue("fields should be in order encountered - 333 occurs before 321", resultTags.indexOf("333") < resultTags.indexOf("321"));
+		
+		// repeated fields should be included:  333, 321, 343, 343, 344
+		assertTrue("all instances of repeated fields should be included", resultTags.size() == 5);
+
+		// more on control fields
+		dfResult = getDataFieldsInRange(rec, "001", "010");
+		resultTags.clear();
+		for (DataField df : dfResult) {
+			resultTags.add(df.getTag());
+		}
+		assertTrue("010 is not a control field, so it should be included", resultTags.contains("010"));
+		assertTrue("no control field tags should be included", resultTags.size() == 1);
+		
+	}
+
+	/**
+	 * unit test for org.solrmarc.tools.MarcUtils.getSubfieldStrings
+	 */
+@Test
+	public void testGetSubfieldStrings()
+	{
+		Record rec = new NoSortRecordImpl();
+		
+		String[] fldTagList = new String[]{"650", "651", "653", "650"};
+		for (int i = 0; i < fldTagList.length; i++) {
+			String tag = fldTagList[i];
+			DataField df = new DataFieldImpl(tag, ' ', ' ');
+			df.addSubfield(new SubfieldImpl('a', "first " + tag + "a"));
+			df.addSubfield(new SubfieldImpl('a', "second " + tag + "a"));
+			df.addSubfield(new SubfieldImpl('b', "contents of " + tag + "b"));
+			df.addSubfield(new SubfieldImpl('a', "third " + tag + "a"));
+			df.addSubfield(new SubfieldImpl('c', tag + "c"));
+			rec.addVariableField(df);
+		}
+	
+		List<DataField> dataFields = getDataFieldsInRange(rec, "650", "655");
+		List<String> subaList = getSubfieldStrings(dataFields, "a");
+		// each instance of single subfield should be on list for mult subfields
+		assertTrue("There should be 12 subfield a instances", subaList.size() == 12);
+		assertTrue("There should be third subfield a instances for 650", subaList.contains("third 650a"));
+		assertTrue("There should be third subfield a instances for 653", subaList.contains("third 653a"));
+		assertTrue("No subfield b's should be in list of a's", !subaList.contains("contents of 650b"));
+		assertTrue("No subfield c's should be in list of a's", !subaList.contains("650c"));
+		
+		// duplicate subfield contents should be included
+		int count = 0;
+		for (String suba : subaList) {
+			if (suba.equals("first 650a"))
+				count++;
+		}
+		assertTrue("There should 2 subfield a containing 'first 650a'", count == 2);
+		count = 0;
+		for (String suba : subaList) {
+			if (suba.equals("second 650a"))
+				count++;
+		}
+		assertTrue("There should 2 subfield a containing 'second 650a'", count == 2);
+		
+		// each instance of subfield should be on list for mult subfields
+		List<String> subacList = getSubfieldStrings(dataFields, "ac");
+		assertTrue("Subfield c should be in ac list", subacList.contains("651c"));
+		assertTrue("There should be third subfield a instances for 653", subacList.contains("third 653a"));
+		assertTrue("There should not be any subfield b in ac list", !subacList.contains("contents of 650b"));
+	}
+
+}
