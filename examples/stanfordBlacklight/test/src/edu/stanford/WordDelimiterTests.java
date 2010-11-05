10f7e057d811459fd3719dc1475aecc49dd2baac
ndushay <ndushay@e0eb17dc-060e-4555-9e78-273a5d168c06>
2010-11-05 21:44:43 +0000

    added word delimiter test for red-rose example of SW-388
    git-svn-id: svn+ssh://lyberdev1.stanford.edu/afs/ir/dev/dlss/svn/ngde/solrmarc/trunk@3751 e0eb17dc-060e-4555-9e78-273a5d168c06

---
+++ examples/stanfordBlacklight/test/src/edu/stanford/WordDelimiterTests.java
@@ -0,0 +1,35 @@
+package edu.stanford;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+/**
+ * junit4 tests for WordDelimiterFilter settings
+ * @author Naomi Dushay
+ */
+public class WordDelimiterTests extends AbstractStanfordBlacklightTest 
+{
+
+	/**
+	 * SW-388  red-rose chain   vs.  prisoner in a red-rose chain
+	 */
+@Test
+	public final void testRedRoseHyphenSearch()
+		throws ParserConfigurationException, IOException, SAXException 
+	{
+		String fldName = "title_245a_search";
+		createIxInitVars("wdfSearchTest.mrc");
+	
+		Set<String> docIds = new HashSet<String>(2);
+		docIds.add("5335304");
+		docIds.add("8702148");
+		
+		assertSearchResults(fldName, "red-rose chain", docIds);
+	}
+
+}
