4c20615ae6e1b65c416b7b80d8ec192374ce2d72
Blacklight account on sw-solr-gen <nobody@sw-solr-gen>
2011-07-27 10:10:04 -0700

    update many_incr.sh

--- examples/stanfordBlacklight/scripts/many_incr.sh
+++ examples/stanfordBlacklight/scripts/many_incr.sh
@@ -1,4 +1,11 @@
-`nohup ./pullThenIndexSirsiIncr.sh 110619`
-`nohup ./pullThenIndexSirsiIncr.sh 110620`
-`nohup ./pullThenIndexSirsiIncr.sh 110621`
-`nohup ./pullThenIndexSirsiIncrOpt.sh 110622`
+`nohup ./pullThenIndexSirsiIncr.sh 110717`
+`nohup ./pullThenIndexSirsiIncr.sh 110718`
+`nohup ./pullThenIndexSirsiIncr.sh 110719`
+`nohup ./pullThenIndexSirsiIncr.sh 110720`
+`nohup ./pullThenIndexSirsiIncr.sh 110721`
+`nohup ./pullThenIndexSirsiIncr.sh 110722`
+`nohup ./pullThenIndexSirsiIncr.sh 110723`
+`nohup ./pullThenIndexSirsiIncr.sh 110724`
+`nohup ./pullThenIndexSirsiIncr.sh 110725`
+`nohup ./pullThenIndexSirsiIncr.sh 110726`
+`nohup ./pullThenIndexSirsiIncrOpt.sh 110727`
