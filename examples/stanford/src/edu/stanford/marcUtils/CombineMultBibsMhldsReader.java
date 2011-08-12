efffb909b0b7583d4e7efebe4cb826009dda70aa
Naomi Dushay <ndushay@stanford.edu>
2011-08-12 10:48:51 -0700

    CombineMultBibsMhldsReader written but no test code yet
    Former-commit-id: 9d561ffc98152b0b28ae6ac44d31903e4c743c3c

--- examples/stanford/src/edu/stanford/marcUtils/CombineMultBibsMhldsReader.java
+++ examples/stanford/src/edu/stanford/marcUtils/CombineMultBibsMhldsReader.java
@@ -1,18 +1,13 @@
 package edu.stanford.marcUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Comparator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.marc4j.MarcPermissiveStreamReader;
-import org.marc4j.MarcReader;
-import org.marc4j.marc.Record;
-import org.marc4j.marc.VariableField;
-import org.solrmarc.tools.MarcUtils;
-import org.solrmarc.tools.StringNaturalCompare;
+import org.marc4j.*;
+import org.marc4j.marc.*;
+import org.solrmarc.tools.*;
 
 /**
  * Binary Marc records have a maximum size of 99999 bytes.  This is true for 
@@ -49,33 +44,42 @@ public class CombineMultBibsMhldsReader implements MarcReader
     /** default regular expression (as a string) of MHLD fields to be merged into the bib record */
     public static final String DEFAULT_MHLD_FLDS_TO_MERGE = "852|853|863|866|867|868";
     
-    static Logger logger = Logger.getLogger(CombineMultBibsMhldsReader.class.getName());
+    /** insert any MHLD fields before the first occurrence of this bib field */
+    public static final String DEFAULT_BIB_FLD_TO_INSERT_BEFORE = "999";
 
     /** used to determine if the match field values are equal */
     static Comparator<String> MATCH_FIELD_COMPARATOR = new StringNaturalCompare();
 
+    static Logger logger = Logger.getLogger(CombineMultBibsMhldsReader.class.getName());
 
-    /** the field in the current record to use for matching purposes */
-    private String currentCntlFldToMatch = DEFAULT_FIELD_TO_MATCH;
+
+    /** the field in the first bib rec of a possible set of records to use for matching purposes */
+    private String firstBibFldToMatch = DEFAULT_FIELD_TO_MATCH;
     		
-    /** the field in the look-ahead record to use for matching purposes */
-    private String lookAheadCntlFldToMatch = DEFAULT_FIELD_TO_MATCH;
+    /** the field in a bib look-ahead record to use for matching purposes */
+    private String lookAheadBibFldToMatch = DEFAULT_FIELD_TO_MATCH;
+
+    /** the field in an MHLD look-ahead record to use for matching purposes */
+    private String mhldFldToMatch = DEFAULT_FIELD_TO_MATCH;
 
     /** regular expression (as a string) of Bib fields to be merged into the bib record */
-    private String bibFldsToMerge = null;
+    private String bibFldsToMerge = DEFAULT_BIB_FLDS_TO_MERGE;
     
     /** regular expression (as a string) of MHLD fields to be merged into the bib record */
-    private String mhldFldsToMerge = null;
+    private String mhldFldsToMerge = DEFAULT_MHLD_FLDS_TO_MERGE;
+    
+    /** mhld fields will be inserted before the first occurrence of this field */
+    private String insertMhldB4bibFld = DEFAULT_BIB_FLD_TO_INSERT_BEFORE;
 
     /** what we will use to read the file and look ahead as we go for additional
      *  records that need to be merged into the current one. */
     private MarcReader marcReader = null;
 
-    /** the last Record read, but not yet compared with the following record */
-    Record currentRecord = null;
-    
-    
-    Record nextRecord = null;
+    /** the most recent bib Record read with a new id */
+    Record currentFirstBibRecord = null;
+
+    /** the last record that has been read (so far) in the marc file */
+    Record lastRecordRead = null;
 
     
     boolean verbose = false;
@@ -85,19 +89,23 @@ public class CombineMultBibsMhldsReader implements MarcReader
 	/**
 	 * 
 	 * @param marcRecsFilename - the name of the file to be read
-     * @param currentCntlFldToMatch - the field in the current record to use for matching purposes (null for DEFAULT_CONTROL_FIELD).
-     * @param lookAheadCntlFldToMatch - the field in the look-ahead record to use for matching purposes (null for DEFAULT_CONTROL_FIELD).
+     * @param firstBibCntlFldToMatch - the field in the current record to use for matching purposes (null for DEFAULT_CONTROL_FIELD).
+     * @param lookAheadBibFldToMatch - the field in the look-ahead bib record to use for matching purposes (null for DEFAULT_CONTROL_FIELD).
+     * @param lookAheadMhldFldToMatch - the field in the look-ahead mhld record to use for matching purposes (null for DEFAULT_CONTROL_FIELD).
 	 * @param bibFldsToMerge - a regular expression (as a string) of Bib fields to be merged when there are multiple bib records (e.g. "998|999"; null for DEFAULT_BIB_FLDS_TO_MERGE)
 	 * @param mhldFldsToMerge - a regular expression (as a string) of MHLD fields to be merged into the bib record (e.g. "852|863|866"; null for DEFAULT_MHLD_FLDS_TO_MERGE)
+	 * @param insertMhldB4bibFld - mhld fields will be inserted before the first occurrence of this field (null for DEFAULT_BIB_FLD_TO_INSERT_BEFORE)
 	 * @param permissive - if true, try to recover from errors, including records with errors, when possible
 	 * @param defaultEncoding - possible values are MARC8, UTF-8, UNIMARC, BESTGUESS
 	 * @param toUtf8 - if true, this will convert records in our import file from (MARC8) encoding into UTF-8 encoding on output to index
 	 */
 	public CombineMultBibsMhldsReader(String marcRecsFilename, 
-										String currentCntlFldToMatch, 
-										String lookAheadCntlFldToMatch,
+										String firstBibCntlFldToMatch,
+										String lookAheadBibFldToMatch,
+										String lookAheadMhldFldToMatch,
 										String bibFldsToMerge,
 										String mhldFldsToMerge,
+										String insertMhldB4bibFld,
 										boolean permissive,
 										String defaultEncoding,
 										boolean toUtf8
@@ -106,19 +114,22 @@ public class CombineMultBibsMhldsReader implements MarcReader
 	{
         marcReader = new MarcPermissiveStreamReader(new FileInputStream(new File(marcRecsFilename)), permissive, toUtf8, defaultEncoding);
 		
-		if (currentCntlFldToMatch != null)
-			this.currentCntlFldToMatch = currentCntlFldToMatch;
-		if (lookAheadCntlFldToMatch != null)
-			this.lookAheadCntlFldToMatch = lookAheadCntlFldToMatch;
+        // all of the below are set to defaults at instantiation, so they
+        //   only need to be changed here if non-null value is passed in.
+        
+		if (firstBibCntlFldToMatch != null)
+			this.firstBibFldToMatch = firstBibCntlFldToMatch;
+		if (lookAheadBibFldToMatch != null)
+			this.lookAheadBibFldToMatch = lookAheadBibFldToMatch;
+		if (lookAheadMhldFldToMatch != null)
+			this.mhldFldToMatch = lookAheadMhldFldToMatch;
 		
-		if (bibFldsToMerge == null)
-			this.bibFldsToMerge = DEFAULT_BIB_FLDS_TO_MERGE;
-		else
+		if (bibFldsToMerge != null)
 			this.bibFldsToMerge = bibFldsToMerge;
-		if (mhldFldsToMerge == null)
-			this.mhldFldsToMerge = DEFAULT_MHLD_FLDS_TO_MERGE;
-		else
+		if (mhldFldsToMerge != null)
 			this.mhldFldsToMerge = mhldFldsToMerge;
+		if (insertMhldB4bibFld != null)
+			this.insertMhldB4bibFld = insertMhldB4bibFld;
 	}
 	
 	
@@ -136,7 +147,7 @@ public class CombineMultBibsMhldsReader implements MarcReader
 										) 
 				throws FileNotFoundException
 	{
-		this (marcRecsFilename, null, null, null, null, permissive, defaultEncoding, toUtf8);
+		this (marcRecsFilename, null, null, null, null, null, null, permissive, defaultEncoding, toUtf8);
 	}
 	
 
@@ -146,7 +157,7 @@ public class CombineMultBibsMhldsReader implements MarcReader
 	public CombineMultBibsMhldsReader(String marcRecsFilename) 
 				throws FileNotFoundException
 	{
-		this (marcRecsFilename, null, null, null, null, true, "MARC8", false);
+		this (marcRecsFilename, null, null, null, null, null, null, true, "MARC8", false);
 	}
 
 	
@@ -156,10 +167,9 @@ public class CombineMultBibsMhldsReader implements MarcReader
 	 */
 	public boolean hasNext()
 	{
-        if (currentRecord == null) 
-            currentRecord = next(); 
-
-        return (currentRecord != null);
+    	if (marcReader != null)
+        	return (marcReader.hasNext());
+        return(false);
     }
 
 	
@@ -170,93 +180,114 @@ public class CombineMultBibsMhldsReader implements MarcReader
      */
 	public Record next()
     {
-        Record bibRec = null;
-        /*
-        if (marcReader != null) 
-        {
-// for ALL matching bib records until no match, or until matching mhld or EOF      	
-        	while (marcReader.hasNext())
-        	{
-        		
-        	}
-// do this for ALL bib records until no match, or until matching mhld or EOF      	
-            bibRec = marcReader.next();
-            if (bibRec != null)
-            {
-        		Record matchingBibRec = getMatchingBibRec(bibRec.getControlNumber());
-        		if (matchingBibRec != null)
-                    bibRec = addBibMergeFields(bibRec, matchingBibRec);
-            }
-// then do it for all matching mhld recs until next bib rec or EOF
-        }
-*/
-        
-        return(bibRec);  
+		if (hasNext())
+		{
+			if (lastRecordRead == null && currentFirstBibRecord == null)
+				// we are at the beginning of the file
+				lastRecordRead = marcReader.next();
+
+            currentFirstBibRecord = lastRecordRead; 
+
+            // look for following bib or mhld records that need to be merged in
+			String idToMatch = MarcUtils.getControlFieldData(currentFirstBibRecord, firstBibFldToMatch);
+			try
+			{
+				mergeFollowingRecs(idToMatch);
+				    //    marcReader is moved to the next record
+				    //    lastRecordRead gets a new value
+				    //    currentFirstBibRecord can get more fields merged in
+			}
+			catch (SolrMarcException e)
+			{
+				e.printStackTrace();
+			    logger.error("STOPPING PROCESSING.");
+			    System.err.println("STOPPING PROCESSING.");
+				System.exit(1);
+			}
+			catch (Exception e)
+			{
+			    logger.error("Couldn't read record after " + idToMatch + " -- " + e.toString(), e);
+			}
+		}
+		
+		return currentFirstBibRecord;
     }
-    
-    
-    
+
+	
     /**
-     * Look for a record in the MHLD file that matches the bibId.  Note that "matching"
-     *  means the ids match, where id is from Record.getControlNumber()
+     * Get next record in the file.  If the designated fields match
+     *   (firstBibFldToMatch, lookAheadBibFldToMatch, mhldFldToMatch), then
+     *   merge the matching record fields into the currentFirstBibRecord
+     *   (bibFldsToMerge, mhldFldsToMerge) and return true.  Otherwise, return
+     *   false.
+     *   
+     *  Side Effects:
+     *    marcReader is moved to the next record
+     *    lookAheadRecord is set to the next bib record that does NOT match the current first bib id
+     *    currentFirstBibRecord can get more fields
      *  
-     * @param bibRecID - the id to match
-     * @return Set of RawRecord objects corresponding to MHLD records that match
-     *  the bibId
+     * @param idToMatch - the id to match the next bib or mhld 
+     * @return true if we found matches and merged the appropriate fields into currentFirstBibRecord; false if we found no matches
+     * @throws SolrMarcException - if file records aren't in ascending ID order, 
+     *  where ID is determined from the firstBibFldToMatch, the lookAheadBibFldToMatch and the mhldFldToMatch
      */
-    private Record getMatchingMhldRec(String bibRecID)
+    private boolean mergeFollowingRecs(String idToMatch)
+    		throws SolrMarcException
     {
-    	Record result = null;
-/*    	
-    	int compareResult = ID_COMPARATOR.compare(currentMhldRec.getControlNumber(), bibRecID);
-
-// String currMhldId = currentMhldRec.getControlNumber();   // useful for Debugging
-    	
-    	if (compareResult > 0)
-    		// MHLD id is after bib id:  we're done and we do not advance in MHLD file
-    		return result;
-    	else
-    	{
-        	if (compareResult == 0)
-            	// current MHLD matches the bibRec - we're done
-        		return currentMhldRec;
-
-    		// proceed to next MHLD record if it's not the last MHLD in the file
-        	// NOTE:  THIS is where the assumption that the bib file is in ascending ID order is made
-    		if (mhldRecCombiningRdr.hasNext())
+    	boolean stillLooking = true;
+    	boolean mergedSome = false;
+		if (stillLooking && hasNext())
+		{
+    		lastRecordRead = marcReader.next();
+    		if (MarcUtils.isMHLDRecord(lastRecordRead)) 
     		{
-	    		currentMhldRec = getNextMhld();
-	    		return getMatchingMhldRec(bibRecID);
+    			String mhldMatchId = MarcUtils.getControlFieldData(lastRecordRead, mhldFldToMatch);
+            	int compareResult = MATCH_FIELD_COMPARATOR.compare(mhldMatchId, idToMatch);
+            	// if it's an mhld, it should match, otherwise it's an error
+            	if (compareResult == 0)
+            	{
+                	// we have a match - merge the mhld into the bib
+            		currentFirstBibRecord = MarcUtils.combineRecords(currentFirstBibRecord, lastRecordRead, mhldFldsToMerge, insertMhldB4bibFld);
+            		mergedSome = true;
+            	}
+            	else 
+               	{
+   				    String errmsg = "Mhld record " + mhldMatchId + " came after bib record " + idToMatch + ": file isn't sorted.  Cannot read file further.";
+   				    logger.error(errmsg);
+   				    throw new SolrMarcException(errmsg);
+               	}
+    			
     		}
-    	}
-*/
-    	return result;
+    		else // bib record
+    		{
+    			String lookAheadBibRecId = MarcUtils.getControlFieldData(lastRecordRead, lookAheadBibFldToMatch);
+            	int compareResult = MATCH_FIELD_COMPARATOR.compare(lookAheadBibRecId, idToMatch);
+            	if (compareResult > 0) 
+            	{
+            		// the new bib id sorts after the current bib id; we're done
+            		stillLooking = false;
+            	}
+            	else
+            	{
+                	if (compareResult == 0)
+                	{
+                    	// we have a match - merge the second bib into the first
+                		currentFirstBibRecord = MarcUtils.combineRecords(currentFirstBibRecord, lastRecordRead, bibFldsToMerge);
+                		mergedSome = true;
+                	}
+                	else // the new bib id sorts before the current one - the records in the file aren't ordered
+                	{
+    				    String errmsg = "Bib record " + lookAheadBibRecId + " came after bib record " + idToMatch + ": file isn't sorted.  Cannot read file further.";
+    				    logger.error(errmsg);
+    				    throw new SolrMarcException(errmsg);
 
+                	}
+            	}    			
+    		}
+		}
+		
+    	return mergedSome;
     }
-
-    
-// FIXME: only remove bib fields Once    
-    /**
-     * given a MARC bib record as a Record object, and a MARC MHLD record as
-     *  a Record object, merge the MHLD fields (indicated in class var
-     *  mhldFldsToMerge) into the bib record, first removing any of those fields
-     *  already existing in the bib record.
-     * @param bibRecord
-     * @param mhldRecord
-     * @return the bib record with the MHLD fields merged in prior to the 999
-     */
-    private Record addMhldFieldsToBibRec(Record bibRecord, Record mhldRecord)
-    {
-        List<VariableField> lvf = bibRecord.getVariableFields(mhldFldsToMerge.split("[|]"));
-        for (VariableField vf : lvf)
-        {
-            bibRecord.removeVariableField(vf);
-        }
-        bibRecord = MarcUtils.combineRecords(bibRecord, mhldRecord, mhldFldsToMerge, "999");
-        return(bibRecord);
-    }
-    
-
-    
+	    
 	
 }
