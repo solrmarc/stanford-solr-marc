52dc8ad5453401de5fc2e9de4997b86e0a00f04d
Naomi Dushay <ndushay@stanford.edu>
2011-08-18 15:42:05 -0700

    correct the exception handling to achieve correct behavior; add tests for unreadable record (first bib and last bib only); allow non-permissive reading for tests
    Former-commit-id: fdf388ea7524d522a2209282df0ac5f4bb898e08

---
+++ test/src/org/solrmarc/tools/LoggerAppender4Testing.java
@@ -0,0 +1,51 @@
+package org.solrmarc.tools;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.util.*;
+import static org.junit.Assert.fail;
+
+/**
+ * used to test what is written to log.  Shamelessly cribbed from 
+ *   http://giordano.scalzo.biz/2009/10/21/no-more-excuses-junit-testing-log-messages/
+ *   http://stackoverflow.com/questions/1827677/how-to-do-a-junit-assert-on-a-message-in-a-logger
+ * @author Naomi Dushay
+ */
+public class LoggerAppender4Testing extends AppenderSkeleton {
+    private final List<LoggingEvent> logEventList = new ArrayList<LoggingEvent>();
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
+
+    @Override
+    protected void append(final LoggingEvent loggingEvent) {
+        logEventList.add(loggingEvent);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    public List<LoggingEvent> getLog() {
+        return new ArrayList<LoggingEvent>(logEventList);
+    }
+    
+    /**
+     * look through all the log messages for the expected string.
+     * @param expected  we are looking for a log message that contains this string
+     */
+    public void assertLogContains(String expected) 
+    {
+    	for (LoggingEvent logEvent : logEventList) 
+    	{
+    		String actual = logEvent.getRenderedMessage();
+    		if (actual.contains(expected))
+    			return;
+    	}
+    	fail("Log did not contain expected string: " + expected);
+    }
+
+}
