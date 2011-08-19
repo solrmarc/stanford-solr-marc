ec3aa9ecec1bf45ef80e60b6c580a020e6ca6232
Naomi Dushay <ndushay@stanford.edu>
2011-08-19 12:14:54 -0700

    remove duplicate bib fields before merging mhld fields; add to AllTests

--- examples/stanford/test/src/edu/stanford/AllTests.java
+++ examples/stanford/test/src/edu/stanford/AllTests.java
@@ -16,6 +16,7 @@ import org.junit.runners.Suite;
         CallNumLoppingUnitTests.class,
         CallNumTopFacetTests.class,
         CallNumUtilsLoppingUnitTests.class,
+        CombineMultBibsMhldsReaderTest.class,
         DatabaseAZSubjectTests.class,
         DiacriticTests.class,
         FormatDatabaseTests.class,
