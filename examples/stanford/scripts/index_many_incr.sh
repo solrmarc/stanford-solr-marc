cab6c498ff84769796ceabe9be8b5d7eee30ab32
Blacklight account on sw-solr-gen <nobody@sw-solr-gen>
2011-11-21 16:45:29 -0800

    add script to index multiple incremental updates
    Former-commit-id: 610784a20a26cdb19aa613893fa785c226f7899b

---
+++ examples/stanford/scripts/index_many_incr.sh
@@ -0,0 +1,10 @@
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110918
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110919
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110920
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110921
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110922
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110923
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110924
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110925
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncr.sh 110926
+/home/blacklight/solrmarc-sw/examples/stanford/scripts/pullThenIndexSirsiIncrOpt.sh 110927
