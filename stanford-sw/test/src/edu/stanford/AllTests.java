89858bf68bae239e9c6eef5f27c07d76ef437216
Naomi Dushay <ndushay@stanford.edu>
2012-01-04 18:58:33 -0800

    stanford alltests now includes all formerly passing tests - w00t
    Former-commit-id: 1807b77e5c8536426124d1345e64f1c2b0699437

--- stanford-sw/test/src/edu/stanford/AllTests.java
+++ stanford-sw/test/src/edu/stanford/AllTests.java
@@ -11,46 +11,46 @@ import org.junit.runners.Suite;
         CallNumberTests.class,
 //        CallNumLaneJacksonTests.class // bad values
         CallNumLCLoppingUnitTests.class,
-//works        CallNumLibLocComboLopTests.class,
+        CallNumLibLocComboLopTests.class,
 //        CallNumLongestComnPfxTests.class // bad values one test using dewey instead of LC?
         CallNumLoppingUnitTests.class,
 //        CallNumTopFacetTests.class // bad values
         CallNumUtilsLoppingUnitTests.class,
 //        CombineMultBibsMhldsReaderTest.class // unreadable rec between isn't working right
-//works        DatabaseAZSubjectTests.class, 
-//works        DiacriticTests.class,
-//works        FormatDatabaseTests.class,
-//works        FormatTests.class,
-//works        GeographicFacetTests.class,
-//works        IncrementalUpdateTests.class,
+        DatabaseAZSubjectTests.class, 
+        DiacriticTests.class,
+        FormatDatabaseTests.class,
+        FormatTests.class,
+        GeographicFacetTests.class,
+        IncrementalUpdateTests.class,
 //        ItemDisplayCallnumLoppingTests.class //bad value for one test
 //        ItemInfoTests.class // bad value for one test
-//works        ItemLACTests.class,
-//works        ItemMissingTests.class,
-//works        ItemNoCallNumberTests.class,
+        ItemLACTests.class,
+        ItemMissingTests.class,
+        ItemNoCallNumberTests.class,
 //        ItemObjectTests.class // bad value for one test
-//works        ItemOnlineTests.class,
-//works        ItemSkippedTests.class,
-//works        ItemsSplitTests.class,
+        ItemOnlineTests.class,
+        ItemSkippedTests.class,
+        ItemsSplitTests.class,
         ItemUtilsUnitTests.class,
-//works        LanguageTests.class,
-//works        MarcCombiningReaderTests.class,
+        LanguageTests.class,
+        MarcCombiningReaderTests.class,
         MergeMhldFldsIntoBibsReaderTests.class,
 //        MhldMappingTests.class // probs
 //        MiscellaneousFieldTests.class // I think it should be using Combine... reader, not MarcCombining reader
 //        NoteFieldsTests.class // one test  bad value
-//works        PhysicalTests.class,
+        PhysicalTests.class,
 //        PublicationTests.class, // bad sorts, etc.
-//works        SeriesTests.class,
-//works        StandardNumberTests.class,
-        SubjectSearchTests.class
+        SeriesTests.class,
+        StandardNumberTests.class,
+        SubjectSearchTests.class,
 //        SubjectTests.class // bad val counts one test
-//works        TitleSearchTests.class,
-//works        TitleSearchVernTests.class,
-//works        TitleTests.class,
-//works        UrlTests.class,
-//works        VernFieldsTests.class,
-//works        WordDelimiterTests.class
+        TitleSearchTests.class,
+        TitleSearchVernTests.class,
+        TitleTests.class,
+        UrlTests.class,
+        VernFieldsTests.class,
+        WordDelimiterTests.class
         })
 
         
