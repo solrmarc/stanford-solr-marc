807ac673368c16f0eac56d6b91a49cb37428fce3
Naomi Dushay <ndushay@stanford.edu>
2012-01-18 15:03:09 -0800

    getDefaultConfig no longer works because no longer using oneJar thingy. Refactored minor ways while I learned this
    Former-commit-id: 20655e24f4d9821bca9e8d8c8a2d33f1ac7c7c02

--- core/src/org/solrmarc/tools/GetDefaultConfig.java
+++ core/src/org/solrmarc/tools/GetDefaultConfig.java
@@ -1,9 +1,9 @@
 package org.solrmarc.tools; 
-import java.io.IOException; 
-import java.util.Enumeration; 
-import java.util.jar.JarFile; 
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.jar.Manifest; 
-import java.util.zip.ZipEntry; 
 
 public class GetDefaultConfig 
 { 
@@ -13,22 +13,23 @@ public class GetDefaultConfig
      */ 
     public static void main(String[] args) 
     {
-        String configProperties = GetDefaultConfig.getConfigName("");
+        String configProperties = GetDefaultConfig.getConfigPropsFileName("");
         System.out.println(configProperties); 
     }
 
     /** 
      * Extract the manifest attribute Default-Config-File from the top level jar file 
-     * @throws IOException 
+     *  I think this is fooched now that I'm not using "onejar"
+     * @deprecated 
      */
-
     public static String getJarFileName()
     { 
         String jarFilename = null; 
         Class<?> bootClass;
         try
         {
-            bootClass = Class.forName("com.simontuffs.onejar.Boot");
+//        	bootClass = Class.forName("com.simontuffs.onejar.Boot");
+            bootClass = Class.forName("org.solrmarc.marc.MarcImporter");
             jarFilename = bootClass.getMethod("getMyJarPath").invoke(null).toString();
         }
         catch (Exception e)
@@ -39,47 +40,77 @@ public class GetDefaultConfig
         return(jarFilename);
     }
     
+// FIXME: code duplicates MarcHandler.getConfigPropsFileName()    
     /** 
      * Extract the manifest attribute Default-Config-File from the top level jar file 
-     * @throws IOException 
      */
+    public static String getConfigPropsFileName(String defaultValue)
+    {
+    	String configPropsFileName = null;
 
-    public static String getConfigName(String defaultValue)
-    { 
-        String configProperties = null; 
-        String jar = getJarFileName();
-        if (jar != null)
+    	// look for it in the manifest of the first jar loaded
+    	Object obj = new Object();
+        ClassLoader classLoader = obj.getClass().getClassLoader();
+        URL manifestUrl = classLoader.getResource("META-INF/MANIFEST.MF");
+        Manifest manifest = null;
+        try
         {
-            try { 
-                JarFile jarFile = new JarFile(jar); 
-                Manifest manifest = jarFile.getManifest(); 
-                String defConfig = manifest.getMainAttributes().getValue("Default-Config-File"); 
-                if (defConfig != null && defConfig.length() > 0) 
-                { 
-                    configProperties = defConfig; 
-                } 
-                else 
-                { 
-                    Enumeration entries = jarFile.entries(); 
-                    while (entries.hasMoreElements()) 
-                    { 
-                        ZipEntry entry = (ZipEntry)entries.nextElement(); 
-                        if (entry.getName().contains("config.properties")) 
-                        { 
-                            configProperties = entry.getName(); 
-                        } 
-                    } 
-                } 
-            } 
-            catch (Exception e) 
-            { 
-                // no manifest property defining the config 
+        	manifest = new Manifest (manifestUrl.openStream());
+        }
+        catch (IOException e)
+        {
+        	//ignore;
+        }
+        if (manifest != null)
+        	configPropsFileName = manifest.getMainAttributes().getValue("Config-Properties-File");
+        
+        // if we didn't find it, look for the first file ending _config.properties in the jar's classpath
+        if (configPropsFileName == null || configPropsFileName.length() == 0)
+        {
+        	// look for file ending in _config.properties
+            //Get the URLs
+            URL[] urls = ((URLClassLoader) classLoader).getURLs();
+            for (int i=0; i< urls.length; i++)
+            {
+            	String fname = urls[i].getFile();
+            	if (fname.endsWith("_config.properties"))
+            	{
+            		configPropsFileName = fname;
+            		break;
+            	}
             }
         }
-        if (configProperties == null)
-        {
-            configProperties = defaultValue;
-        }
-        return(configProperties);
+        return(configPropsFileName);
+//    	
+//        String jarName = getJarFileName();
+//        if (jarName != null)
+//        {
+//            try { 
+//                JarFile jarFile = new JarFile(jarName); 
+//                Manifest manifest = jarFile.getManifest(); 
+//                String defConfig = manifest.getMainAttributes().getValue("Config-Properties-File"); 
+//                if (defConfig != null && defConfig.length() > 0) 
+//                    configPropsFileName = defConfig; 
+//                else 
+//                { 
+//                    Enumeration entries = jarFile.entries(); 
+//                    while (entries.hasMoreElements()) 
+//                    { 
+//                        ZipEntry entry = (ZipEntry)entries.nextElement(); 
+//                        if (entry.getName().contains("config.properties")) 
+//                            configPropsFileName = entry.getName(); 
+//                    } 
+//                } 
+//            } 
+//            catch (Exception e) 
+//            { 
+//                // no manifest property defining the config 
+//            }
+//        }
+//        
+//        if (configPropsFileName == null)
+//            configPropsFileName = defaultValue;
+//
+//        return(configPropsFileName);
     }
 }
