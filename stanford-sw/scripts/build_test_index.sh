477931cb3bd58e9d4ef81f87747cd3d96c1701a6
Naomi Dushay <ndushay@stanford.edu>
2012-01-23 13:37:15 -0800

    updating two indexing scripts

--- stanford-sw/scripts/build_test_index.sh
+++ stanford-sw/scripts/build_test_index.sh
@@ -20,7 +20,6 @@ JAVA_HOME=/usr/lib/jvm/java
 # set up the classpath
 DIST_DIR=$SOLRMARC_BASEDIR/dist
 SITE_JAR=$DIST_DIR/StanfordSearchWorksSolrMarc.jar
-#CORE_JAR=$DIST_DIR/SolrMarc.jar
 CP=$SITE_JAR:$DIST_DIR:$DIST_DIR/lib
 
 # create log directory
@@ -29,7 +28,7 @@ LOG_DIR=$LOG_PARENT_DIR/$LOG_SUBDIR
 mkdir -p $LOG_DIR
 
 # index the file
-java -Xmx1g -Xms1g  -cp $CP -jar $SITE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
-#java -Xmx1g -Xms1g -Dsolr.data.dir=$SOLR_DATA_DIR -cp $CP -Dsolr.optimize_at_end="true" -jar $CORE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
+java -Xmx1g -Xms1g -cp $CP -jar $SITE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
+#java -Xmx1g -Xms1g -cp $CP -Dsolr.optimize_at_end="true" -jar $SITE_JAR $RAW_DATA_DIR/physicalTests.mrc &>$LOG_DIR/log.txt
 
 exit 0
