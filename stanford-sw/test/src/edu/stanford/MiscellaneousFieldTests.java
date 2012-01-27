ccb13f90ee0967be9b8a810b2855cab64ef3924f
Naomi Dushay <ndushay@stanford.edu>
2012-01-26 17:52:25 -0800

    don't put marc21 into index; only marcxml

--- stanford-sw/test/src/edu/stanford/MiscellaneousFieldTests.java
+++ stanford-sw/test/src/edu/stanford/MiscellaneousFieldTests.java
@@ -17,18 +17,18 @@ import static org.junit.Assert.*;
  * junit4 tests for Stanford University revisions to solrmarc
  * @author Naomi Dushay
  */
-public class MiscellaneousFieldTests extends AbstractStanfordTest 
+public class MiscellaneousFieldTests extends AbstractStanfordTest
 {
 	/**
 	 * Test correct document id - the id is from 001 with an a in front
 	 */
-@Test 
-	public final void testId() 
+@Test
+	public final void testId()
 		throws ParserConfigurationException, SAXException, IOException, SolrServerException
 	{
 		String fldName = "id";
 		createFreshIx("idTests.mrc");
-		
+
         int numDocs = getNumMatchingDocs("collection", "sirsi");
         assertEquals("Number of documents in index incorrect: ", 3, numDocs);
         assertDocNotPresent("001noSubNo004");
@@ -39,50 +39,50 @@ public class MiscellaneousFieldTests extends AbstractStanfordTest
         assertDocNotPresent("004noSuba");
         assertDocPresent("001subaAnd004suba");
         assertDocNotPresent("004suba");
-        
+
         assertSingleResult("001suba", fldName, "\"001suba\"");
         assertSingleResult("001subaAnd004nosub", fldName, "\"001subaAnd004nosub\"");
         assertSingleResult("001subaAnd004suba", fldName, "\"001subaAnd004suba\"");
 	}
-	
+
 
 	/**
 	 * Test that there is no field created when the translation map is missing
 	 *  the value to be mapped and when the map has value set to null
 	 */
 @Test
-	public final void testMapMissingValue() 
-			throws ParserConfigurationException, IOException, SAXException, SolrServerException 
+	public final void testMapMissingValue()
+			throws ParserConfigurationException, IOException, SAXException, SolrServerException
 	{
 		String fldName = "language";
 		createFreshIx("langTests.mrc");
-	
+
 		assertZeroResults(fldName, "null");
 		assertZeroResults(fldName, "\\?\\?\\?");
 		assertZeroResults(fldName, "mis");     // 008mis041ak
 		assertZeroResults(fldName, "Miscellaneous languages");
 		assertZeroResults(fldName, "mul");     // 008mul041atha
-		assertZeroResults(fldName, "Multiple languages"); 
+		assertZeroResults(fldName, "Multiple languages");
 		assertZeroResults(fldName, "und");
 		assertZeroResults(fldName, "zxx");
 	}
 
-	
+
 	/**
 	 * Test population of allfields
 	 */
 @Test
-	public final void testAllSearch() 
-			throws ParserConfigurationException, IOException, SAXException, SolrServerException 
+	public final void testAllSearch()
+			throws ParserConfigurationException, IOException, SAXException, SolrServerException
 	{
 		String fldName = "all_search";
 		createFreshIx("allfieldsTests.mrc");
-		
+
 		String docId = "allfields1";
 
 		// 245 just for good measure
-        assertSingleResult(docId, fldName, "should"); 
-        
+        assertSingleResult(docId, fldName, "should");
+
         // 0xx fields are not included except 024, 027, 028
         assertSingleResult(docId, fldName, "2777802000"); // 024
         assertSingleResult(docId, fldName, "90620"); // 024
@@ -94,13 +94,13 @@ public class MiscellaneousFieldTests extends AbstractStanfordTest
         // 3xx fields ARE included
         assertSingleResult(docId, fldName, "sound"); // 300
         assertSingleResult(docId, fldName, "annual");  // 310
-        
+
         // 6xx subject fields - we're including them, even though
         // fulltopic is all subfields of all 600, 610, 630, 650, 655
         // fullgeographic is all subfields of all 651
         //   b/c otherwise standard numbers and other things are doubled here,
         //   but topics are not.
-        
+
         // 9xx fields are NOT included
         assertZeroResults(fldName, "EDATA");  // 946
         assertZeroResults(fldName, "pamphlet");  // 947
@@ -110,26 +110,26 @@ public class MiscellaneousFieldTests extends AbstractStanfordTest
 
 	/**
 	 * display_type is supposed to be a sort of "hidden" facet to allow UI
-	 *  to look at appropriate types of records for different "views" 
+	 *  to look at appropriate types of records for different "views"
 	 *  (e.g.  Images, Maps, Book Reader ...)
 	 */
 @Test
-	public final void testDisplayTypeField() 
+	public final void testDisplayTypeField()
 	    throws ParserConfigurationException, IOException, SAXException, SolrServerException
 	{
 		createFreshIx("idTests.mrc");
 	    String fldName = "display_type";
-	    
+
 	    // all MARC records from Symphony
         assertEquals("docs aren't all display_type sirsi", 3, getNumMatchingDocs(fldName, "sirsi"));
 	}
-	
+
 
 	/**
 	 * open search field should be stored and indexed
 	 */
 @Test
-	public final void testOpenSearch() 
+	public final void testOpenSearch()
 	    throws ParserConfigurationException, IOException, SAXException, SolrServerException
 	{
 		createFreshIx("allfieldsTests.mrc");
@@ -140,18 +140,18 @@ public class MiscellaneousFieldTests extends AbstractStanfordTest
 
 
 	/**
-	 * test preservation of field ordering from marc21 input to marc21 stored in record
+	 * test preservation of field ordering from marc input to marc stored in record
 	 */
 @Test
-	public final void testFieldOrdering() 
-			throws ParserConfigurationException, IOException, SAXException, SolrServerException 
+	public final void testFieldOrdering()
+			throws ParserConfigurationException, IOException, SAXException, SolrServerException
 	{
 		createFreshIx("fieldOrdering.mrc");
 		SolrDocument doc = getDocument("1");
-		String marc21 = (String) doc.getFirstValue("marc21");
+		String marc21 = (String) doc.getFirstValue("marcxml");
 		int ix650 = marc21.indexOf("650first");
 		int ix600 = marc21.indexOf("600second");
 		assertTrue("fields are NOT in the original order", ix650 < ix600);
 	}
-	
+
 }
