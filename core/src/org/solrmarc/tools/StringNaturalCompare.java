2d716c0ebdcc7299bed5803085b167c42f3a177b
Naomi Dushay <ndushay@stanford.edu>
2012-07-26 13:48:30 -0700

    avoid null pointer exceptions for StringNaturalCompare

--- core/src/org/solrmarc/tools/StringNaturalCompare.java
+++ core/src/org/solrmarc/tools/StringNaturalCompare.java
@@ -7,26 +7,27 @@ public class StringNaturalCompare implements Comparator<String>
 
     public int compare(String s1, String s2)
     {
-        int result = strnatcmp0(s1, s2, true);
-        return(result);
+        return(strnatcmp0(s1, s2, true));
     }
 
 
+    /* The longest run of digits wins.  That aside, the greatest
+   value wins, but we can't know that it will until we've scanned
+   both numbers to know that they have the same magnitude, so we
+   remember it in BIAS. */
     private static int compareRight(String s1, int ind1, String s2, int ind2)
     {
-         int bias = 0;
-         
-         /* The longest run of digits wins.  That aside, the greatest
-        value wins, but we can't know that it will until we've scanned
-        both numbers to know that they have the same magnitude, so we
-        remember it in BIAS. */
-         for (;; ind1++, ind2++) 
+        int bias = 0;
+        int s1len = s1 == null ? 0 : s1.length();
+        int s2len = s2 == null ? 0 : s2.length();
+
+         for (;; ind1++, ind2++)
          {
-             if (ind1 == s1.length() && ind2 == s2.length()) 
+             if (ind1 == s1len && ind2 == s2len)
                  return(bias);
-             else if (ind1 == s1.length()) 
+             else if (ind1 == s1len)
                  return(-1);
-             else if (ind2 == s2.length()) 
+             else if (ind2 == s2len)
                  return(+1);
              char a = s1.charAt(ind1);
              char b = s2.charAt(ind2);
@@ -38,27 +39,30 @@ public class StringNaturalCompare implements Comparator<String>
                  return +1;
              else if (a < b)
              {
-                 if (bias == 0)  bias = -1;
-             } 
-             else if (a > b) 
+                 if (bias == 0)
+                	 bias = -1;
+             }
+             else if (a > b)
              {
-                 if (bias == 0)  bias = +1;
-             } 
+                 if (bias == 0)
+                	 bias = +1;
+             }
          }
     }
 
 
+    /* Compare two left-aligned numbers: the first to have a different value wins. */
     public static int compareLeft(String s1, int ind1, String s2, int ind2)
     {
-         /* Compare two left-aligned numbers: the first to have a
-            different value wins. */
-        for (;; ind1++, ind2++) 
+        int s1len = s1 == null ? 0 : s1.length();
+        int s2len = s2 == null ? 0 : s2.length();
+        for (;; ind1++, ind2++)
         {
-            if (ind1 == s1.length() && ind2 == s2.length()) 
+            if (ind1 == s1len && ind2 == s2len)
                 return 0;
-            else if (ind1 == s1.length()) 
+            else if (ind1 == s1len)
                 return(-1);
-            else if (ind2 == s2.length()) 
+            else if (ind2 == s2len)
                 return(+1);
             char a = s1.charAt(ind1);
             char b = s2.charAt(ind2);
@@ -72,48 +76,50 @@ public class StringNaturalCompare implements Comparator<String>
                 return -1;
             else if (a > b)
                 return +1;
-         }         
+         }
     }
-    
+
     public static int strnatcmp0(String s1, String s2, boolean fold_case)
     {
         int ai, bi;
-        char ca, cb;
-        boolean fractional;
         int result;
 
         //   assert(a && b);
         ai = bi = 0;
+        int s1len = s1 == null ? 0 : s1.length();
+        int s2len = s2 == null ? 0 : s2.length();
         while (true)
         {
-            ca = ai < s1.length() ? s1.charAt(ai) : 0;
-            cb = bi < s2.length() ? s2.charAt(bi) : 0;
+            char ca = ai < s1len ? s1.charAt(ai) : 0;
+            char cb = bi < s2len ? s2.charAt(bi) : 0;
 
             /* skip over leading spaces or zeros */
             while (Character.isWhitespace(ca))
             {
                 ai = ai + 1;
-                ca = ai < s1.length() ? s1.charAt(ai) : 0;
+                ca = ai < s1len ? s1.charAt(ai) : 0;
             }
 
             while (Character.isWhitespace(cb))
             {
                 bi = bi + 1;
-                cb = bi < s2.length() ? s2.charAt(bi) : 0;
+                cb = bi < s2len ? s2.charAt(bi) : 0;
             }
 
             /* process run of digits */
             if (Character.isDigit(ca) && Character.isDigit(cb))
             {
-                fractional = (ca == '0' || cb == '0');
+                boolean fractional = (ca == '0' || cb == '0');
 
                 if (fractional)
                 {
-                    if ((result = compareLeft(s1, ai, s2, bi)) != 0) return result;
+                    if ((result = compareLeft(s1, ai, s2, bi)) != 0)
+                    	return result;
                 }
                 else
                 {
-                    if ((result = compareRight(s1, ai, s2, bi)) != 0) return result;
+                    if ((result = compareRight(s1, ai, s2, bi)) != 0)
+                    	return result;
                 }
             }
 
@@ -130,8 +136,10 @@ public class StringNaturalCompare implements Comparator<String>
                 cb = Character.toUpperCase(cb);
             }
 
-            if (ca < cb) return -1;
-            else if (ca > cb) return +1;
+            if (ca < cb)
+            	return -1;
+            else if (ca > cb)
+            	return +1;
 
             ++ai;
             ++bi;
