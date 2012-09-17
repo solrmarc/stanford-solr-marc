9cedd15cdcbd0cd78ddf257393b967fe30c34294
Naomi Dushay <ndushay@stanford.edu>
2012-09-17 16:23:15 -0700

    refactoring pub utils tests and adding tests for new getDateSliderVals method
    Former-commit-id: d600dd9b1a507d4210be809073ca4c71ebd02d53

---
+++ stanford-sw/test/src/edu/stanford/PublicationUtilsUnitTests.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2012.  The Board of Trustees of the Leland Stanford Junior University. All rights reserved.
+ *
+ * Redistribution and use of this distribution in source and binary forms, with or without modification, are permitted provided that: The above copyright notice and this permission notice appear in all copies and supporting documentation; The name, identifiers, and trademarks of The Board of Trustees of the Leland Stanford Junior University are not used in advertising or publicity without the express prior written permission of The Board of Trustees of the Leland Stanford Junior University; Recipients acknowledge that this distribution is made available as a research courtesy, "as is", potentially with defects, without any obligation on the part of The Board of Trustees of the Leland Stanford Junior University to provide support, services, or repair;
+ *
+ * THE BOARD OF TRUSTEES OF THE LELAND STANFORD JUNIOR UNIVERSITY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, WITH REGARD TO THIS SOFTWARE, INCLUDING WITHOUT LIMITATION ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND IN NO EVENT SHALL THE BOARD OF TRUSTEES OF THE LELAND STANFORD JUNIOR UNIVERSITY BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, TORT (INCLUDING NEGLIGENCE) OR STRICT LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+package edu.stanford;
+
+import static edu.stanford.PublicationUtils.*;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.*;
+
+import org.marc4j.marc.*;
+
+/**
+ * @author Naomi Dushay
+ *
+ */
+public class PublicationUtilsUnitTests
+{
+	private MarcFactory factory = MarcFactory.newInstance();
+
+	/**
+	 * assure pub_date field ignores the unknown-ish phrases
+	 */
+@Test
+	public void testGetValidPubDateIgnores264Unknowns()
+	{
+		List<DataField> f264list = new ArrayList<DataField>();
+	    DataField df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "[Date of publication not identified] :"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "[Date of Production not identified]"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "Date of manufacture Not Identified"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "[Date of distribution not identified]"));
+	    f264list.add(df);
+	    assertNull("getValidPubDate should return null when it only finds 264 sub c with ignored values ", getValidPubDate("0000", null, f264list));
+	}
+
+
+	/**
+	 * assure pub dates later than limit are ignored
+	 */
+@Test
+	public void testGetValidPubDateIgnoresTooLate()
+	{
+		List<DataField> f264list = new ArrayList<DataField>();
+	    DataField df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "9999"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "6666"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "22nd century"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "8610s"));
+	    f264list.add(df);
+
+	    int upperLimit = 2012;
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', Integer.toString(upperLimit + 1)));
+	    f264list.add(df);
+
+	    assertNull("getValidPubDate should return null when it only finds 264c with vals that are greater than the upper limit", getValidPubDate("0000", upperLimit, 500, null, f264list));
+	}
+
+
+	/**
+	 * assure pub dates of < 500 are ignored
+	 */
+@Test
+	public void testGetValidPubDateIgnoresTooEarly()
+	{
+		List<DataField> f264list = new ArrayList<DataField>();
+	    DataField df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "0000"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "0036"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "0197"));
+	    f264list.add(df);
+	    df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "0204"));
+	    f264list.add(df);
+	    int lowerLimit = 500;
+	    assertNull("getValidPubDate should return null when it only finds 264c with vals that are < the lower limit", getValidPubDate("0000", 2022, lowerLimit, null, f264list));
+	}
+
+
+	/**
+	 * test that auto-correction of pub date in 008 by checking value in 260c
+	 */
+@Test
+	public void testGetPubDateAutoCorrectsWith260c()
+	{
+	    assertEquals("getValidPubDate should return its fourth argument when the first argument isn't a valid date", "2009", getValidPubDate("0059", "2009.", new ArrayList()));
+	    assertEquals("getValidPubDate should return its fourth argument when the first argument isn't a valid date", "1970", getValidPubDate("0197", "[197?]", new ArrayList()));
+	    assertEquals("getValidPubDate should return its fourth argument when the first argument isn't a valid date", "2004", getValidPubDate("0204", "[2004]", new ArrayList()));
+	    assertNull("getValidPubDate should return null when it only finds 260c with invalid date", getValidPubDate("0019", "invalid", new ArrayList()));
+	}
+
+
+@Test
+	public void test008DDDDForPubDateSlider()
+	{
+		ControlField f008 = factory.newControlField("008", "      b1960    ");
+		Set<String> result = getPubDateSliderVals(f008, null, null);
+		assertEquals("getPubDateSliderVals should have a single result", 1, result.size());
+		assertEquals("getPubDateSliderVals should get date from 008 bytes 7-10 when 4 digits", "1960", result.toArray()[0]);
+	}
+
+@Test
+	public void test008DDDUForPubDateSlider()
+	{
+		ControlField f008 = factory.newControlField("008", "      b196u    ");
+		Set<String> result = getPubDateSliderVals(f008, null, null);
+		assertEquals("getPubDateSliderVals should have a single result", 1, result.size());
+		assertEquals("getPubDateSliderVals should get date ending in 0 from 008 bytes 7-10 when dddu", "1960", result.toArray()[0]);
+	}
+@Test
+	public void test008DDUUForPubDateSlider()
+	{
+		ControlField f008 = factory.newControlField("008", "      b19uu    ");
+		Set<String> result = getPubDateSliderVals(f008, null, null);
+		assertEquals("getPubDateSliderVals should have a single result", 1, result.size());
+		assertEquals("getPubDateSliderVals should get date ending in 00 from 008 bytes 7-10 when dduu", "1900", result.toArray()[0]);
+	}
+@Test
+	public void test008DUUUForPubDateSlider()
+	{
+		ControlField f008 = factory.newControlField("008", "      b1uuu    ");
+		Set<String> result = getPubDateSliderVals(f008, null, null);
+		assertEquals("getPubDateSliderVals should have no results from 008 bytes 7-10 when duuu and no other options", 0, result.size());
+	}
+
+//@Test
+	public void test008DateBad()
+	{
+		ControlField f008 = factory.newControlField("008", "      bnope    ");
+		Set<String> result = getPubDateSliderVals(f008, null, null);
+		assertEquals("getPubDateSliderVals should have no results if bad 008 and no other options", 0, result.size());
+
+		result = getPubDateSliderVals(f008, "1960", new ArrayList());
+		assertEquals("getPubDateSliderVals should get single result from second arg if 008 has bad value", "1960", result.toArray()[0]);
+
+		List<DataField> f264list = new ArrayList<DataField>();
+	    DataField df = factory.newDataField("264", ' ', ' ');
+	    df.addSubfield(factory.newSubfield('c', "1970"));
+	    f264list.add(df);
+		result = getPubDateSliderVals(f008, null, f264list);
+		assertEquals("getPubDateSliderVals should get single result from third arg if 008 has bad value and no second arg", "1970", result.toArray()[0]);
+	}
+
+//@Test
+	public void	testIndexMultipleDates()
+	{
+		fail("implement me");
+	}
+
+//@Test
+	public void testIndexOneDate()
+	{
+		Record record = factory.newRecord();
+		record.addVariableField(factory.newControlField("008", "      b1960"));
+		fail("implement me");
+	}
+
+//@Test
+	public void testMdate2is9999()
+	{
+		fail("implement me");
+	}
+
+}
