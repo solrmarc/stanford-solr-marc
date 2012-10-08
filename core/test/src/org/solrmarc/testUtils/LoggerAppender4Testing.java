279360c46ad08049c4f92e120e824f5b1be999c2
Naomi Dushay <ndushay@stanford.edu>
2012-10-08 15:47:42 -0700

    add assertLogContains(level, msgstr) method to LoggerAppender4Testing class so you can check the log message level as well as the message contents
    Former-commit-id: e90c4baa6514646bbe7b6fc15975193e70baf566

--- core/test/src/org/solrmarc/testUtils/LoggerAppender4Testing.java
+++ core/test/src/org/solrmarc/testUtils/LoggerAppender4Testing.java
@@ -1,13 +1,14 @@
 package org.solrmarc.testUtils;
 
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
 import java.util.*;
 import static org.junit.Assert.fail;
 
 /**
- * used to test what is written to log.  Shamelessly cribbed from 
+ * used to test what is written to log.  Shamelessly cribbed from
  *   http://giordano.scalzo.biz/2009/10/21/no-more-excuses-junit-testing-log-messages/
  *   http://stackoverflow.com/questions/1827677/how-to-do-a-junit-assert-on-a-message-in-a-logger
  * @author Naomi Dushay
@@ -32,14 +33,14 @@ public class LoggerAppender4Testing extends AppenderSkeleton {
     public List<LoggingEvent> getLog() {
         return new ArrayList<LoggingEvent>(logEventList);
     }
-    
+
     /**
      * look through all the log messages for the expected string.
      * @param expected  we are looking for a log message that contains this string
      */
-    public void assertLogContains(String expected) 
+    public void assertLogContains(String expected)
     {
-    	for (LoggingEvent logEvent : logEventList) 
+    	for (LoggingEvent logEvent : logEventList)
     	{
     		String actual = logEvent.getRenderedMessage();
     		if (actual.contains(expected))
@@ -48,4 +49,19 @@ public class LoggerAppender4Testing extends AppenderSkeleton {
     	fail("Log did not contain expected string: " + expected);
     }
 
+    /**
+     * look through all the log messages for the expected string.
+     * @param expLevel the expected level of the log message containing the string
+     * @param expStr  we are looking for a log message that contains this string
+     */
+    public void assertLogContains(Level expLevel, String expStr)
+    {
+    	for (LoggingEvent logEvent : logEventList)
+    	{
+    		String actual = logEvent.getRenderedMessage();
+    		if (actual.contains(expStr) && logEvent.level.equals(expLevel))
+    			return;
+    	}
+    	fail("Log did not contain a " + expLevel.toString() + " message with expected string: " + expStr);
+    }
 }
