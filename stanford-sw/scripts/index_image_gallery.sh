6cf5294febf930981a9601cd4b55e0c9c0eb9998
sw-solr-g blacklight <blacklight@sw-solr-g.stanford.edu>
2012-10-09 15:51:39 -0700

    change image gallery script to use localhost

--- stanford-sw/scripts/index_image_gallery.sh
+++ stanford-sw/scripts/index_image_gallery.sh
@@ -3,7 +3,7 @@
 #
 # Naomi Dushay 2011-11-22
 
-#curl http://sw-solr-gen:8983/solr/update?commit=true -H 'Content-type:text/xml; charset=utf-8' --data-binary @filename
-curl http://sw-solr-gen:8983/solr/update -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/reid-dennis/ReidDennisSolrDocs_20110722_0001.xml
-curl http://sw-solr-gen:8983/solr/update -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/kolb/KolbSolrDocs_20110722_0001.xml
-curl http://sw-solr-gen:8983/solr/update?commit=true -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/kolb/KolbSolrDocs_20110722_0002.xml
+#curl http://localhost:8983/solr/update?commit=true -H 'Content-type:text/xml; charset=utf-8' --data-binary @filename
+curl http://localhost:8983/solr/update -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/reid-dennis/ReidDennisSolrDocs_20110722_0001.xml
+curl http://localhost:8983/solr/update -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/kolb/KolbSolrDocs_20110722_0001.xml
+curl http://localhost:8983/solr/update?commit=true -H 'Content-type:text/xml; charset=utf-8' --data-binary @/data/image_gallery/kolb/KolbSolrDocs_20110722_0002.xml
