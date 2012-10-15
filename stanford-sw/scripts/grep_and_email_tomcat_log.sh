a1c0504533a1709fdefdd41304258eb7fde8a589
sw-solr-m blacklight <blacklight@sw-solr-m.stanford.edu>
2012-10-15 14:26:49 -0700

    finish grep_and_email_tomcat_log.sh script again
    Former-commit-id: 57404971b23fd32243d084c8f64199f09ba632c4

--- stanford-sw/scripts/grep_and_email_tomcat_log.sh
+++ stanford-sw/scripts/grep_and_email_tomcat_log.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 grep 'ERROR\|WARN\|FATAL' /var/log/tomcat6/catalina.out | mail -s 'solr log warning, error and fatal messages' searchworks-reports@lists.stanford.edu, datacontrol@stanford.edu
