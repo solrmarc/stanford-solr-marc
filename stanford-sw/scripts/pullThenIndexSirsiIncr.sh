d6e15f9f449069a3dab8335f1d191c1ca62d1fef
sw-solr-m blacklight <blocklight@sw-solr-m.stanford.edu>
2012-10-16 11:26:44 -0700

    use full path for grep script within other scripts

--- stanford-sw/scripts/pullThenIndexSirsiIncr.sh
+++ stanford-sw/scripts/pullThenIndexSirsiIncr.sh
@@ -56,7 +56,7 @@ nohup java -Xmx4g -Xms4g $DEL_ARG -Dsolr.commit_at_end="true" -cp $CP -jar $SITE
 # email the results
 mail -s 'pullThenIndexSirsiIncr.sh output' searchworks-reports@lists.stanford.edu, datacontrol@stanford.edu < $LOG_FILE
 # email the solr log messages 
-#./grep_and_email_tomcat_log.sh
+$SOLRMARC_BASEDIR/stanford-sw/scripts/grep_and_email_tomcat_log.sh
 
 # include latest course reserves data
 JRUBY_OPTS="--1.9"
