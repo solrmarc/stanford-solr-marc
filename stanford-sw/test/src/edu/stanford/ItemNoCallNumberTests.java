cd796593f14e01576f5d82d228a8a2fd1c74be3f
Naomi Dushay <ndushay@stanford.edu>
2013-02-27 09:33:16 -0800

    changes for biz to come into Symphony

--- stanford-sw/test/src/edu/stanford/ItemNoCallNumberTests.java
+++ stanford-sw/test/src/edu/stanford/ItemNoCallNumberTests.java
@@ -7,37 +7,36 @@ import org.junit.*;
 import edu.stanford.enumValues.CallNumberType;
 
 /**
- * junit4 tests for Stanford University 
+ * junit4 tests for Stanford University
  *   cope properly with items with call number "NO CALL NUMBER" - display, discoverable  SW-231
  * @author Naomi Dushay
  */
-public class ItemNoCallNumberTests extends AbstractStanfordTest 
+public class ItemNoCallNumberTests extends AbstractStanfordTest
 {
 	private String fileName = "itemNoCallNumber.mrc";
     private String testFilePath = testDataParentPath + File.separator + fileName;
 
 @Before
-	public final void setup() 
+	public final void setup()
 	{
 		mappingTestInit();
-	}	
+	}
 
-	//  If there is no call number in any 999 for the record 
+	//  If there is no call number in any 999 for the record
   	//   look for one in the 050, 090, 086 (see below)
 
 	// search results - do not display:   item_display    lopped callnum
-	// record view - do not display:   item_display   full callnum, sort callnum 
+	// record view - do not display:   item_display   full callnum, sort callnum
 	// availability - display (don't call jenson)
 	// facets  - assign if there is a call number in the bib record
 	// shelflist  - if call number, include
 	//   (preferred_barcode, shelfkey, reversekey, item_display  shelfkey, reversekey)
     //   NOTE:  display call number as starting point, but display the link text in the list
     //
-	
+
 	/**
 	 * "NO CALL NUMBER" is a skipped call number;  there are resources that
-	 *   only have items with this call number, especially from Lane and
-	 *   Jackson (and especially online items from Lane and Jackson)
+	 *   only have items with this call number, especially from Lane (and especially online items from Lane)
 	 */
 @Test
 	public void testNoCallNumber()
@@ -51,7 +50,7 @@ public class ItemNoCallNumberTests extends AbstractStanfordTest
 		fldName = "preferred_barcode";
 		solrFldMapTest.assertNoSolrFld(testFilePath, id, fldName);
 		solrFldMapTest.assertSolrFldValue(testFilePath, id050, fldName, "LL271310");
-		
+
 		fldName = "shelfkey";
 		solrFldMapTest.assertNoSolrFld(testFilePath, id, fldName);
 		String callnum = "A1 .B2";
@@ -63,12 +62,12 @@ public class ItemNoCallNumberTests extends AbstractStanfordTest
 		String uncallnum = "NO CALL NUMBER";
 // TODO: should  these items have "NO CALL NUMBER" callnum in lopped, full and volsort?  SW-231
 //  or should these sections be empty?
-		String firstPart = "LL271310" + sep + "LANE-MED" + sep + "ASK@LANE" + sep + sep + sep;	
+		String firstPart = "LL271310" + sep + "LANE-MED" + sep + "ASK@LANE" + sep + sep + sep;
 	    String fldVal = firstPart + uncallnum + sep + sep + sep + uncallnum + sep;
 	    solrFldMapTest.assertSolrFldHasNoValue(testFilePath, id, fldName, fldVal);
 	   	fldVal = firstPart + sep + sep + sep + sep;
 	    solrFldMapTest.assertSolrFldValue(testFilePath, id, fldName, fldVal);
-	    
+
 		String rkey = org.solrmarc.tools.CallNumUtils.getReverseShelfKey(skey).toLowerCase();
 	    fldVal = firstPart + uncallnum + sep + skey + sep + rkey + uncallnum + sep;
 		solrFldMapTest.assertSolrFldHasNoValue(testFilePath, id050, fldName, fldVal);
