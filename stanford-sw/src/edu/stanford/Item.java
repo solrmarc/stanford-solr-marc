7305f3b33a1683ac307df0b1a0d903322015fc68
Naomi Dushay <ndushay@stanford.edu>
2013-02-27 09:33:16 -0800

    changes for biz to come into Symphony
    Former-commit-id: b9ab2b0762ed4a3233ec6647e0ec484c4336bfe3

--- stanford-sw/src/edu/stanford/Item.java
+++ stanford-sw/src/edu/stanford/Item.java
@@ -120,6 +120,13 @@ public class Item {
 		else
 			hasShelbyLoc = false;
 
+		if (library == "BUSINESS"
+				&& (StanfordIndexer.BIZ_SHELBY_LOCS.contains(currLoc)
+						|| StanfordIndexer.BIZ_SHELBY_LOCS.contains(homeLoc) ) )
+			hasBizShelbyLoc = true;
+		else
+			hasBizShelbyLoc = false;
+
 		if (StanfordIndexer.SKIPPED_CALLNUMS.contains(rawCallnum)
 				|| rawCallnum.startsWith(ECALLNUM)
 				|| rawCallnum.startsWith(TMP_CALLNUM_PREFIX))
