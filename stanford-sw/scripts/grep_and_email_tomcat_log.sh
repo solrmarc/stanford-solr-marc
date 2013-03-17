26d6b623c9f9ccfa92bbf754718af0bca2aa043a
sw-solr-g blacklight <blacklight@sw-solr-g.stanford.edu>
2013-03-17 13:25:58 -0700

    fix grep_and_email_tomcat_log.sh to write to temp .txt file for windoze mime type issues

--- stanford-sw/scripts/grep_and_email_tomcat_log.sh
+++ stanford-sw/scripts/grep_and_email_tomcat_log.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-grep 'ERROR\|FATAL' /var/log/tomcat6/catalina.out | tac | mail -s 'solr log error and fatal messages' searchworks-reports@lists.stanford.edu, datacontrol@stanford.edu
+grep 'ERROR\|FATAL' /var/log/tomcat6/catalina.out | tac > /data/sirsi/latest/updates/logs/tmp.txt
+mail -s 'solr log error and fatal messages' searchworks-reports@lists.stanford.edu, datacontrol@stanford.edu < /data/sirsi/latest/updates/logs/tmp.txt
