a2b35dc61386115fb6634f6457ccc7985d645495
Naomi Dushay <ndushay@stanford.edu>
2013-06-05 16:47:39 -0700

    add script for soft_commit

---
+++ stanford-sw/scripts/soft_commit.sh
@@ -0,0 +1,4 @@
+# change value below 
+# do a "soft commit" - "this will refresh the 'view' of the index in a more performant manner, but without "on-disk" guarantees"
+#   http://wiki.apache.org/solr/UpdateXmlMessages#A.22commit.22_and_.22optimize.22
+curl "https://localhost:8983/solr/update" -H "Content-Type: application/xml" --data-binary '<commit softCommit="true"/>'
