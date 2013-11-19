7a92de25a569da52ffbb52bfd3e168bbe94690f1
Naomi Dushay <ndushay@stanford.edu>
2013-11-19 13:13:51 -0800

    retain old format field values for SW URL continuity

--- stanford-sw/src/edu/stanford/FormatUtils.java
+++ stanford-sw/src/edu/stanford/FormatUtils.java
@@ -32,7 +32,7 @@ public class FormatUtils {
 	 * @param cf008 - the 008 field as a ControlField object
 	 * @param Set of Strings containing Format enum values per the given data
 	 */
-	static Set<String> getFormatsPerLdrAnd008(String leaderStr, ControlField cf008)
+	static Set<String> getFormatsPerLdrAnd008Old(String leaderStr, ControlField cf008)
 	{
 		Set<String> result = new HashSet<String>();
 
@@ -94,6 +94,114 @@ public class FormatUtils {
 				result.add(Format.BOOK.toString());
 			break;
 		} // end switch
+
+/* not yet vetted  2010-10-30
+		// is it an updating database? (leader/07 = "s" or "i" and 008/21 = "d") OR (006/00 = "s" and 006/04 = "d")
+		// (leader/07 = "s" and 008/21 = "d" handled by getSerialFormat()
+		// (006/00 = "s" and 006/04 = "d") is handled by getSerialFormat() which calls getSerialFormat006()
+
+		if (leaderChar07 == 'i') {
+			// check if it's a database based on 008 char 21
+			char c21 = '\u0000';
+			if (cf008 != null) {
+				c21 = ((ControlField) cf008).getData().charAt(21);
+				if (c21 == 'd')
+					result.add(Format.DATABASE_OTHER.toString());
+			}
+		}
+*/
+
+		return result;
+	}
+
+
+	/**
+	 * Assign formats based on leader chars 06, 07 and chars in 008
+	 *
+	 * Algorithms for formats are currently in email  message from Vitus Tang to
+	 *  Naomi Dushay, cc Phil Schreur, Margaret Hughes, and Jennifer Vine
+	 *  dated July 23, 2008.
+	 *
+	 * @param leaderStr - the leader field, as a String
+	 * @param cf008 - the 008 field as a ControlField object
+	 * @param Set of Strings containing Format enum values per the given data
+	 */
+	static Set<String> getFormatsPerLdrAnd008(String leaderStr, ControlField cf008)
+	{
+		Set<String> result = new HashSet<String>();
+
+		// Note: MARC21 documentation refers to char numbers that are 0 based,
+		// just like java string indexes, so char "06" is at index 6, and is
+		// the seventh character of the field
+
+		// assign formats based on leader chars 06, 07 and chars in 008
+		char leaderChar07 = leaderStr.charAt(7);
+		char leaderChar06 = leaderStr.charAt(6);
+		switch (leaderChar06) {
+		case 'a':
+			if (leaderChar07 == 'a' || leaderChar07 == 'm')
+				result.add(Format.BOOK.toString());
+			break;
+		case 'b':
+		case 'p':
+			result.add(Format.MANUSCRIPT_ARCHIVE.toString());
+			break;
+		case 'c':
+		case 'd':
+			result.add(Format.MUSIC_SCORE.toString());
+			break;
+		case 'e':
+		case 'f':
+			result.add(Format.MAP_GLOBE.toString());
+			break;
+		case 'g':
+			// look for m or v in 008 field, char 33 (count starts at 0)
+			if (cf008 != null && cf008.find("^.{33}[mv]"))
+				result.add(Format.VIDEO.toString());
+			break;
+		case 'i':
+			result.add(Format.SOUND_RECORDING.toString());
+			break;
+		case 'j':
+			result.add(Format.MUSIC_RECORDING.toString());
+			break;
+		case 'k':
+    		// look for i, k, p, s or t in 008 field, char 33 (count starts at 0)
+			if (cf008 != null && cf008.find("^.{33}[ikpst]"))
+				result.add(Format.IMAGE.toString());
+			break;
+		case 'm':
+			// look for a in 008 field, char 26 (count starts at 0)
+			if (cf008 != null && cf008.find("^.*{26}a"))
+				result.add(Format.COMPUTER_FILE.toString());
+			break;
+		case 'o': // instructional kit
+			result.add(Format.OTHER.toString());
+			break;
+		case 'r': // object
+			result.add(Format.OTHER.toString());
+			break;
+		case 't':
+			if (leaderChar07 == 'a' || leaderChar07 == 'm')
+				result.add(Format.BOOK.toString());
+			break;
+		} // end switch
+
+/* not yet vetted  2010-10-30
+		// is it an updating database? (leader/07 = "s" or "i" and 008/21 = "d") OR (006/00 = "s" and 006/04 = "d")
+		// (leader/07 = "s" and 008/21 = "d" handled by getSerialFormat()
+		// (006/00 = "s" and 006/04 = "d") is handled by getSerialFormat() which calls getSerialFormat006()
+
+		if (leaderChar07 == 'i') {
+			// check if it's a database based on 008 char 21
+			char c21 = '\u0000';
+			if (cf008 != null) {
+				c21 = ((ControlField) cf008).getData().charAt(21);
+				if (c21 == 'd')
+					result.add(Format.DATABASE_OTHER.toString());
+			}
+		}
+*/
 
 		return result;
 	}
@@ -166,6 +274,12 @@ public class FormatUtils {
 	private static String getSerialFormatFromChar(char ch) {
 		if (ch != '\u0000')
 			switch (ch) {
+/* not yet vetted  2010-10-30
+				case 'd': // updating database
+					return Format.DATABASE_OTHER.toString();
+*/
+//				case 'l': // updating looseleaf (ignore)
+//					break;
 				case 'm': // monographic series
 					return Format.BOOK.toString();
 				case 'n':
@@ -176,18 +290,31 @@ public class FormatUtils {
 		return null;
 	}
 
-//	/**
-//	 * @param record - marc4j record object
-//	 * @return true if there is a 245h that contains the string "microform",
-//	 *  false otherwise
-//	 */
-//	static boolean isMicroformat(Record record) {
-//		Set<String> titleH = MarcUtils.getSubfieldDataAsSet(record, "245", "h", " ");
-//		if (Utils.setItemContains(titleH, "microform"))
-//			return true;
-//		else
-//			return false;
-//	}
+	/**
+	 * @param record - marc4j record object
+	 * @return true if there is a 245h that contains the string "microform",
+	 *  false otherwise
+	 */
+	static boolean isMicroformat(Record record) {
+		Set<String> titleH = MarcUtils.getSubfieldDataAsSet(record, "245", "h", " ");
+		if (Utils.setItemContains(titleH, "microform"))
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * @param record - marc4j record object
+	 * @return true if there is a 245h that contains the string "microform",
+	 *  false otherwise
+	 */
+	static boolean isMicroformatOld(Record record) {
+		Set<String> titleH = MarcUtils.getSubfieldDataAsSet(record, "245", "h", " ");
+		if (Utils.setItemContains(titleH, "microform"))
+			return true;
+		else
+			return false;
+	}
 
 	/**
 	 * thesis is determined by the presence of a 502 field.
